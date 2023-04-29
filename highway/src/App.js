ed4644556dcff1dd114b75aa3920b3b3114444f5
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2023-04-29 23:10:17 +0900

    Feat : Routing
    회원별 회원가입페이지 라우팅

--- highway/src/App.js
+++ highway/src/App.js
@@ -3,17 +3,23 @@ import "./App.css";
 import Categories from "./components/Categories";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import OtherSignUp from "./pages/OtherSignUp";
 import Promotion from "./pages/Promotion";
 import SchoolRanking from "./pages/SchoolRanking";
 import Search from "./pages/Search";
-import SIgnUp from "./pages/SignUp";
+import SignUp from "./pages/SignUp";
+import StudentSignUp from "./pages/StudentSignUp";
+import UserProfile from "./pages/UserProfile";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SIgnUp />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/profile" element={<UserProfile />} />
+        <Route path="/signup/student" element={<StudentSignUp />} />
+        <Route path="/signup/other" element={<OtherSignUp />} />
         <Route element={<Categories />}>
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
