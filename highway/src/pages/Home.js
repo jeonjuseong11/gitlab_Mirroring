80780ba63c17e4b4c0261d8a5972b0a1e5451ff8
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2023-04-29 23:11:28 +0900

    Feat : Home
    홈페이지 기본 틀 작성

--- highway/src/pages/Home.js
+++ highway/src/pages/Home.js
@@ -1,8 +1,12 @@
 import React from "react";
 import AppLayout from "../components/AppLayout";
-
+import Slider from "../components/Slider/Slider";
 const Home = () => {
-  return <AppLayout>Home</AppLayout>;
+  return (
+    <AppLayout>
+      <Slider />
+    </AppLayout>
+  );
 };
 
 export default Home;
