169a4def6660ff1eb2755d9bf5197b6fc2498ee2
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2023-05-03 15:18:12 +0900

    Add : store.js
    store 생성

---
+++ highway/src/store/store.js
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import rootSaga from "../sagas";
+import rootReducer from "../reducers";
+import createSagaMiddleware from "redux-saga";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const enhancer =
+  process.env.NODE_ENV === "production"
+    ? compose(applyMiddleware(sagaMiddleware))
+    : composeWithDevTools(applyMiddleware(sagaMiddleware));
+const store = createStore(rootReducer, enhancer);
+
+sagaMiddleware.run(rootSaga); // 루트 사가를 실행해줍니다.
+
+export default store;
