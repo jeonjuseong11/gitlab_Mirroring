19ef79ef4ac248ac0de4c47a26d89da26b26992d
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2023-05-04 12:33:34 +0900

    Feat : baseURL
    백엔드 연결 기본 URL 지정

--- highway/src/sagas/index.js
+++ highway/src/sagas/index.js
@@ -1,6 +1,8 @@
 import { all, fork } from "redux-saga/effects";
 import userSaga from "./user";
+import axios from "axios";
 
+axios.defaults.baseURL = "http://localhost:8080/";
 export default function* rootSaga() {
   yield all([fork(userSaga)]);
 }
