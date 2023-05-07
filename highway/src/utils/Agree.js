e7f9803e8abf4acf78b541ef06bb31d1c14ad5d0
WePung <akgre5@naver.com>
2023-05-07 15:06:08 +0900

    Add : Agree.js
    이용약과 내용 추가

---
+++ highway/src/utils/Agree.js
@@ -0,0 +1,28 @@
+export const items = [
+    {
+        label: "HIGHWAY 이용약관",
+        key: '0'
+    },
+    {
+        type:'divider',
+    },
+    {
+        label: "동의 내용1",
+        key: '2',
+    },
+    {
+        type:'divider',
+    },
+    {
+        label: "개인정보 수집 및 이용 동의",
+        key: '3'
+    },
+    {
+        type:'divider',
+    },
+    {
+        label: "동의 내용1",
+        key: '5',
+    },
+    
+];
