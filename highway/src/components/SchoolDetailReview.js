cbadd75b765d4e4a2d0463f765a14245cae03917
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2023-05-11 10:11:50 +0900

    Feat : SchoolDetailReview.js
    학교 리뷰 페이지

--- highway/src/components/SchoolDetailReview.js
+++ highway/src/components/SchoolDetailReview.js
@@ -1,7 +1,54 @@
 import React from "react";
+import { Col, Row } from "antd";
 
 const SchoolDetailReview = () => {
-  return <div>리뷰</div>;
+  return (
+    <div
+      style={{
+        backgroundColor: "#F2F2F2",
+        paddingTop: "1rem",
+        boxSizing: "border-box",
+        paddingBottom: "1rem",
+      }}
+    >
+      <Row gutter={[16, 16]} style={{ boxSizing: "border-box", justifyContent: "center" }}>
+        <Col span={12}>
+          <div
+            style={{
+              backgroundColor: "white",
+              borderRadius: "5px",
+              height: "10rem",
+              marginBottom: "1rem",
+            }}
+          >
+            학교 별점
+          </div>
+
+          <div
+            style={{
+              backgroundColor: "white",
+              borderRadius: "5px",
+              height: "20rem",
+            }}
+          >
+            한줄평
+          </div>
+        </Col>
+
+        <Col span={10}>
+          <div
+            style={{
+              backgroundColor: "white",
+              borderRadius: "5px",
+              height: "31rem",
+            }}
+          >
+            뭐할까
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default SchoolDetailReview;
