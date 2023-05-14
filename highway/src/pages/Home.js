2aafff893d3e17523b17e5388c9a6d546eaafbd6
WePung <akgre5@naver.com>
2023-05-15 01:12:53 +0900

    Edit : Home.js
    Home Component 출력 요소 수정

--- highway/src/pages/Home.js
+++ highway/src/pages/Home.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Slider from "../components/Slider/AdSlider";
 import AppLayout from "../components/AppLayout";
 import SearchForm from "../components/SearchForm";
 import CardList from "../components/Card/CardList";
+import { LOAD_USER_REQUEST } from "../constants/actionTypes";
+import { useDispatch } from "react-redux";
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const userInfo = JSON.parse(localStorage.getItem("USER_INFO"));
+  const [filterValue, setFilterValue] = useState("");
+  useEffect(() => {
+    console.log(userInfo);
+    if (userInfo) {
+      dispatch({
+        type: LOAD_USER_REQUEST,
+        data: userInfo.userNo,
+      });
+    }
+  }, [userInfo]);
   return (
     <AppLayout>
       <Slider />
-      <SearchForm />
-      <CardList />
+      <SearchForm filterValue={filterValue} setFilterValue={setFilterValue} />
+      <CardList filterValue={filterValue} />
     </AppLayout>
   );
 };
