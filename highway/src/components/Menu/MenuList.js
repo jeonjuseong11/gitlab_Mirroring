810735048925551f8d32ffdd9397844e06f63d24
WePung <akgre5@naver.com>
2023-05-15 01:09:41 +0900

    Add : MenuList.js
    MenuList.js에 CSS 추가

--- highway/src/components/Menu/MenuList.js
+++ highway/src/components/Menu/MenuList.js
@@ -1,5 +1,10 @@
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
+export const MenuWrapper = styled.header`
+  border-top: 1px solid #f2f2f2;
+  border-bottom: 1px solid #f2f2f2;
+`;
 export const MenuLists = [
   //메뉴 리스트 key : Menu 컴포넌트에 해당 항목 밑줄 쳐짐
   {
