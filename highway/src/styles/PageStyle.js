51d77940fca3023290f0b4e323d5ff33ae3913f9
WePung <akgre5@naver.com>
2023-05-15 01:17:24 +0900

    Edit : PageStyle.js
    Page Css 수정

--- highway/src/styles/PageStyle.js
+++ highway/src/styles/PageStyle.js
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
   text-align: center;
   width: 60%;
   margin: 0 auto;
+`;
+export const SubWrapper = styled.div`
+  text-align: center;
+  width: 100%;
+  margin: 0 auto;
+`;
+export const NoDecoLink = styled(Link)`
+  float: right;
+  text-decoration: none;
+  color: black;
+  margin-right: 5px;
 `;
