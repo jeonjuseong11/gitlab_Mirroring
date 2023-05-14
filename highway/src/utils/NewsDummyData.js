8878b01e10166b8d5b7a0bf6d3cde6e8a728fee1
WePung <akgre5@naver.com>
2023-05-15 01:17:50 +0900

    Add : DummyData
    News, Video DummyData 추가

---
+++ highway/src/utils/NewsDummyData.js
@@ -0,0 +1,52 @@
+export const NewsDummyData = [
+    {
+        newsId : 1,
+        newsTitle : "당신의 '아침 루틴' 궁금하다",
+        newsContent : "뉴스 기사",
+        writer : "HIGHWAY TIMES",
+        src :"https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+        newsViews: 132,
+    },
+    {
+        newsId : 2,
+        newsTitle : "당신의 '교복 스탠다드' 궁금하다",
+        newsContent : "뉴스 기사",
+        writer : "HIGHWAY TIMES",
+        src : "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+        newsViews: 10,
+    },
+    {
+        newsId : 3,
+        newsTitle : "당신의 '스터디 스타일' 궁금하다",
+        newsContent : "뉴스 기사",
+        writer : "HIGHWAY TIMES",
+        src : "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+        newsViews: 12,
+    },
+    {
+        newsId : 4,
+        newsTitle : "당신의 '직업 적성' 궁금하다",
+        newsContent : "뉴스 기사",
+        writer : "HIGHWAY TIMES",
+        src : "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+        newsViews: 15,
+    },
+    {
+        newsId : 5,
+        newsTitle : "당신의 '취미 생활' 궁금하다",
+        newsContent : "뉴스 기사",
+        writer : "HIGHWAY TIMES",
+        src : "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+        newsViews: 16,
+    },
+    {
+        newsId : 6,
+        newsTitle : "당신의 '등교 루트' 궁금하다",
+        newsContent : "뉴스 기사",
+        writer : "HIGHWAY TIMES",
+        src : "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+        newsViews: 17,
+    },
+]
+
+export default NewsDummyData;
