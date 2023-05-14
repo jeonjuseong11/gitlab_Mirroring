d82b77e491edb0488b66d38c85b131c2a3d15480
WePung <akgre5@naver.com>
2023-05-15 01:08:36 +0900

    Edit : TopMenu.js
    TopMenu Component 수정

--- highway/src/components/Menu/TopMenu.js
+++ highway/src/components/Menu/TopMenu.js
@@ -1,35 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import Header from "../Header";
 import { Menu } from "antd";
 import { Wrapper } from "../../styles/PageStyle";
 import { MenuLists } from "./MenuList";
-import styled from "styled-components";
-
-const MenuHeader = styled.header`
-  border-top: 1px solid #c2c2c2;
-  border-bottom: 1px solid #c2c2c2;
-`;
+import { MenuWrapper } from "./MenuList";
 
 const TopMenu = () => {
   const location = useLocation();
+  const [testUrl, setTestUrl] = useState("/");
   useEffect(() => {
-    // console.log(location.pathname);
+    if (
+      location.pathname === "/promotion" ||
+      location.pathname === "/promotion/news" ||
+      location.pathname === "/promotion/videos"
+    ) {
+      setTestUrl("/promotion");
+    } else {
+      setTestUrl(location.pathname);
+    }
   }, [location]);
   return (
     <div>
       <Wrapper>
         <Header />
       </Wrapper>
-      <MenuHeader>
+      <MenuWrapper>
         <Wrapper>
-          <Menu
-            mode="horizontal"
-            items={MenuLists}
-            selectedKeys={location.pathname}
-          />
+          <Menu mode="horizontal" items={MenuLists} selectedKeys={testUrl} />
         </Wrapper>
-      </MenuHeader>
+      </MenuWrapper>
       <main>
         <Outlet />
       </main>
