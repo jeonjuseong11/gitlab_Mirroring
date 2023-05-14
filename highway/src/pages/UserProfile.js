663d33b6f03d1305bd4c7f7f1278868e656406d6
WePung <akgre5@naver.com>
2023-05-15 01:17:01 +0900

    Add : UserProfile Page
    UserProfile Page 추가

--- highway/src/pages/UserProfile.js
+++ highway/src/pages/UserProfile.js
@@ -1,7 +1,30 @@
+import { Button, Form, Input } from "antd";
+import axios from "axios";
 import React from "react";
 
 const UserProfile = () => {
-  return <div>UserProfile</div>;
+  const [form] = Form.useForm();
+
+  const onFinish = (values) => {
+    console.log(values);
+    axios.post(`http://localhost:8080/todo?`);
+  };
+  return (
+    <div>
+      원래 프로필 페이지임
+      <br />
+      <Form form={form} name="test" onFinish={onFinish}>
+        <Form.Item name="tokenTest">
+          <Input defaultValue="토큰 테스트용입니다" />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            test
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 };
 
 export default UserProfile;
