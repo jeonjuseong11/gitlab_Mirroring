0589554faf46b75992baada688eeb57bc4c2f283
WePung <akgre5@naver.com>
2023-05-16 01:14:16 +0900

    Edit : Promotion.js
    안쓰이는 코드 제거

--- highway/src/pages/Promotion.js
+++ highway/src/pages/Promotion.js
@@ -1,7 +1,6 @@
 import { Menu } from "antd";
 import React from "react";
 import { Outlet, useLocation } from "react-router-dom";
-import PromotionHome from "../components/Promotion/PromotionHome";
 import RecommendList from "../components/Promotion/RecommendList";
 import { PromotionWrapper } from "../styles/PromotionStyle";
 import { items } from "../utils/PromotionList";
@@ -14,7 +13,6 @@ const Promotion = () => {
       <RecommendList />
       <Menu mode="horizontal" items={items} selectedKeys={location.pathname} />
       <Outlet />
-      <PromotionHome />
     </PromotionWrapper>
   );
 };
