47fb1d81467a27fd1df1db50d7e34202334054b9
WePung <akgre5@naver.com>
2023-05-16 01:16:31 +0900

    Edit : PromotionHome.js
    NewDummyData수정으로 인한 코드 수정

--- highway/src/components/Promotion/PromotionHome.js
+++ highway/src/components/Promotion/PromotionHome.js
@@ -2,33 +2,49 @@ import React from "react";
 import AppLayout from "../AppLayout";
 import newsList from "../../utils/NewsDummyData";
 import { Link, useLocation } from "react-router-dom";
-import { Image, List, Menu } from "antd";
+import { Image, List } from "antd";
+import { NewsDummyData as items } from "../../utils/NewsDummyData";
+import {
+  PromotionHomeSection,
+  PromotionHomeTitleListWrapper,
+  PromotionHomeWrapper,
+  PromtionHomeSectionImage,
+} from "../../styles/PromotionStyle";
 
 const PromotionHome = () => {
+  const random = Math.floor(Math.random() * items.length);
   const location = useLocation();
   return (
-    <div>
-      <AppLayout>
+    <PromotionHomeWrapper>
+      <PromotionHomeSection>
         <h2>첫 번째 test 제목</h2>
-        <List
-          itemLayout="horizontal"
-          dataSource={newsList}
-          renderItem={(item) => (
-            <List.Item>
-              <Image preview={false} width={277} height={122} src={item.src} />
-              <List.Item.Meta
-                title={
-                  <Link to={`/promotion/news/${item.newsId}`}>
-                    {item.newsTitle}
-                  </Link>
-                }
-                description={item.newsContent}
-              />
-            </List.Item>
-          )}
-        />
-      </AppLayout>
-    </div>
+        <div>
+          <PromtionHomeSectionImage
+            width={280}
+            height={210}
+            src={`${items[random].src}`}
+          />
+        </div>
+        <PromotionHomeTitleListWrapper>
+          <List
+            itemLayout="horizontal"
+            dataSource={newsList}
+            renderItem={(item) => (
+              <List.Item>
+                <List.Item.Meta
+                  title={
+                    <Link to={`/promotion/news/${item.newsId}`}>
+                      {item.newsTitle}
+                    </Link>
+                  }
+                  description={item.newsContent}
+                />
+              </List.Item>
+            )}
+          />
+        </PromotionHomeTitleListWrapper>
+      </PromotionHomeSection>
+    </PromotionHomeWrapper>
   );
 };
 
