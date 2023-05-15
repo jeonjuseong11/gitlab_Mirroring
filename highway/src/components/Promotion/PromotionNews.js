80cc81249bca9e16ea403feca4bb436208947d84
WePung <117412075+WePung@users.noreply.github.com>
2023-05-15 16:34:03 +0900

    Edit : PromotionNews.js
    컴포넌트 병합으로 인한 수정

--- highway/src/components/Promotion/PromotionNews.js
+++ highway/src/components/Promotion/PromotionNews.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import AppLayout from '../AppLayout';
-import PromotionNewsItem from './PromotionNewsItem';
+import React from "react";
+import AppLayout from "../AppLayout";
+import newsList from "../../utils/NewsDummyData";
+import { Link } from "react-router-dom";
+import { Image, List } from "antd";
 
 const PromotionNews = () => {
   return (
     <AppLayout>
-      <PromotionNewsItem />
+      <h2>첫 번째 test 제목</h2>
+      <List
+        itemLayout="horizontal"
+        dataSource={newsList}
+        renderItem={(item) => (
+          <List.Item>
+            <Image preview={false} width={277} height={122} src={item.src} />
+            <List.Item.Meta
+              title={
+                <Link to={`/promotion/news/${item.newsId}`}>
+                  {item.newsTitle}
+                </Link>
+              }
+              description={item.newsContent}
+            />
+          </List.Item>
+        )}
+      />
     </AppLayout>
   );
 };
