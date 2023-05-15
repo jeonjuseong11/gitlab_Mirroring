2de754ae03a00691921a46de452600afda94f595
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2023-05-15 19:24:54 +0900

    Delete : postReducer,postSaga
    post 관련 saga,reducer 삭제

--- highway/src/reducers/index.js
+++ highway/src/reducers/index.js
@@ -2,13 +2,11 @@ import { combineReducers } from "redux";
 
 import user from "./user";
 import school from "./school";
-import post from "./post";
 
 // (이전상태, 액션) => 다음상태
 const rootReducer = combineReducers({
   user,
   school,
-  post,
 });
 
 export default rootReducer;
--- highway/src/sagas/index.js
+++ highway/src/sagas/index.js
@@ -3,11 +3,10 @@ import axios from "axios";
 
 import userSaga from "./user";
 import schoolSaga from "./school";
-import postSaga from "./post";
 
 axios.defaults.baseURL = "http://localhost:8080/";
 // axios.defaults.headers["Access-Control-Allow-Origin"] = "*";
 // axios.defaults.withCredentials = true;
 export default function* rootSaga() {
-  yield all([fork(userSaga), fork(schoolSaga), fork(postSaga)]);
+  yield all([fork(userSaga), fork(schoolSaga)]);
 }
