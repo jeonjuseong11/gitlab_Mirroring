2563cb68ab25daa16cc5aa6a8ea7b67c9d2661c2
WePung <117412075+WePung@users.noreply.github.com>
2023-05-15 16:31:49 +0900

    Edit : PromotionStyle.js
    PromotionStyle CSS 수정

--- highway/src/styles/PromotionStyle.js
+++ highway/src/styles/PromotionStyle.js
@@ -1,23 +1,29 @@
 import { Image, Form } from "antd";
 import styled from "styled-components";
 
-
 export const PromotionWrapper = styled(Form)`
-width: 50%;
-margin: 1% Auto;
+  width: 50%;
+  margin: 1% Auto;
 `;
 
 // RecommendList.js
 export const RecommendWrapper = styled.div`
-width: 100%;
-background-color: #FFFFFF;
-
+  width: 100%;
+  background-color: #ffffff;
 `;
 export const RecommendImage = styled(Image)`
-border-radius: 5%;
-`
-
+  border-radius: 5%;
+`;
+export const TestHeadLineWrapper = styled.div`
+  width: 700px;
+  height: 400px;
+    .src});
+  border-radius: 15px;
+  background-color: red;
+`;
 // RecommendHeadLine.js
 export const RecommendHeadLineWrapper = styled.div`
-float:right;
-`
+  float: right;
+`;
+// PromotionNewsDetail.js
+export const PromotionNewsDetailWrapper = styled.div``;
