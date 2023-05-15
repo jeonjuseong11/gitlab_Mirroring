dcf2ed4ccaaad800f5490491fc1f57a1d4e95ed6
WePung <akgre5@naver.com>
2023-05-16 01:16:05 +0900

    Edit : RecommendList.js
    출력 요소 변경

--- highway/src/components/Promotion/RecommendList.js
+++ highway/src/components/Promotion/RecommendList.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { TestHeadLineWrapper } from "../../styles/PromotionStyle";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { NewsDummyData as items } from "../../utils/NewsDummyData";
+import { Image } from "antd";
 
 const RecommendList = () => {
-  const location = useLocation();
-  console.log(location.pathname);
-
+  const random = Math.floor(Math.random() * items.length);
   return (
     <div>
       <h1>HIGHWAY TIMES</h1>
       <TestHeadLineWrapper>
-        <Link to={"/"}>
-          <h2>제목</h2>
-          <p>기사</p>
+        <Link to={`news/${random}`}>
+          <Image
+            width={700}
+            height={400}
+            preview={false}
+            src={`${items[random].src}`}
+          />
         </Link>
       </TestHeadLineWrapper>
     </div>
