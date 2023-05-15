986418c08a12ab1381400acf21bafa8a2896ff58
WePung <117412075+WePung@users.noreply.github.com>
2023-05-15 16:33:14 +0900

    Edit : PromotionNewsDetail.js
    PromotionNewsDetail Component 간략화

--- highway/src/components/Promotion/PromotionNewsDetail.js
+++ highway/src/components/Promotion/PromotionNewsDetail.js
@@ -1,36 +1,41 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import newsList from "../../utils/NewsDummyData"
-import { Image } from 'antd';
+import React from "react";
+import { Link, useLocation, useParams } from "react-router-dom";
+import newsList from "../../utils/NewsDummyData";
+import { Image } from "antd";
 
 const PromotionNewsDetail = () => {
+  const location = useLocation();
+  // news테이블이 없기에 임시방편으로 세워둠
+  const { newsId } = useParams();
+  const id = parseInt(newsId);
 
-    const location = useLocation();
-    // news테이블이 없기에 임시방편으로 세워둠
-    const text = window.location.href;
-    const url = parseInt(text.replace("http://localhost:3000/promotion/news/",""));
-    console.log(url)
-    const test = newsList.map((it)=>{
-        if(it.newsId === url){
-        return(
-            <div key={it.newsId}>
-                <div><Image width={753} height={424} preview={false} src={it.src}/></div>
-                <div>{it.newsTitle}</div>
-                <div>{it.newsContent}</div>
-            </div>
-        )
-        }
-    })
-    return (
+  return (
+    <div>
+      <div>
         <div>
-            {test}
-            <div>
-                <Link to={`http://localhost:3000/promotion/news/${url-1}`}><button>이전</button></Link>
-                <Link to={`http://localhost:3000/promotion/news/`}><button>목록으로</button></Link>
-                <Link to={`http://localhost:3000/promotion/news/${url+1}`}><button>다음</button></Link>
-            </div>
+          <Image
+            width={753}
+            height={424}
+            preview={false}
+            src={newsList[id].src}
+          />
         </div>
-    );
+        <div>{newsList[id].newsTitle}</div>
+        <div>{newsList[id].newsContent}</div>
+      </div>
+      <div>
+        <Link to={`http://localhost:3000/promotion/news/${id - 1}`}>
+          <button>이전</button>
+        </Link>
+        <Link to={`http://localhost:3000/promotion/news/`}>
+          <button>목록으로</button>
+        </Link>
+        <Link to={`http://localhost:3000/promotion/news/${id + 1}`}>
+          <button>다음</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default PromotionNewsDetail;
