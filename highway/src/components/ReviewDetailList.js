a9c56af1f3c2e031387ca71624b7762eddc8e285
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2023-05-16 10:50:24 +0900

    CSS : ReviewDetailList
    상세 리뷰 리스트 css 수정

--- highway/src/components/ReviewDetailList.js
+++ highway/src/components/ReviewDetailList.js
@@ -1,5 +1,6 @@
-import { Avatar, Button, List, Rate } from "antd";
-import React, { useEffect } from "react";
+import { StarFilled } from "@ant-design/icons";
+import { Avatar, List, Rate } from "antd";
+import React from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
@@ -9,6 +10,9 @@ const DetailReviewsWrapper = styled.div`
   text-align: left;
   font-size: 0.5rem;
   margin-top: 1rem;
+  width: 6.5rem;
+  float: left;
+  margin-left: 1rem;
 `;
 const DetailReviewP = styled.p`
   margin: 0;
@@ -18,26 +22,29 @@ const DetailReviewP = styled.p`
 `;
 const DetailReviewAvatarWrapper = styled.div`
   display: flex;
-  gap: 1rem;
+  gap: 5rem;
   align-items: center;
   height: 2rem;
+  margin-left: 1rem;
 `;
 const DetailReviewUserTagsWrapper = styled.div`
   display: flex;
-  margin-left: 3.5rem;
+  margin-left: 9rem;
 `;
 const DetailReviewContentWrapper = styled.div`
   color: black;
   word-break: break-all;
-  position: relative;
-  left: -10vw;
-  top: -1rem;
+  height: 11vh;
+  float: left;
+  width: 60%;
+  display: inline-block;
+  text-align: left;
+  margin-left: 1.5rem;
+  margin-top: 1rem;
 `;
 const ReviewDetailList = () => {
   const { schoolId } = useParams();
-  const { reviews } = useSelector(
-    (state) => state.school.schools[schoolId - 1]
-  );
+  const { reviews } = useSelector((state) => state.school.schools[schoolId - 1]);
   console.log(reviews);
   return (
     <>
@@ -52,13 +59,24 @@ const ReviewDetailList = () => {
           pageSize: 2,
         }}
         renderItem={(item) => (
-          <List.Item>
+          <List.Item key={item.id}>
             <List.Item.Meta
               title={
                 <>
                   <DetailReviewAvatarWrapper>
-                    <Avatar size={40}>{item.author[0]}</Avatar>
+                    <Avatar size={50} style={{ marginTop: "2rem" }}>
+                      {item.author[0]}
+                    </Avatar>
                     <h3>{item.author}</h3>
+                    <span>
+                      <StarFilled style={{ color: "#FFDC82", marginLeft: "-5rem" }} />
+                      {(item.rate.trafficRate +
+                        item.rate.facilityRate +
+                        item.rate.cafeteriaRate +
+                        item.rate.educationRate +
+                        item.rate.employmentRate) /
+                        5}
+                    </span>
                   </DetailReviewAvatarWrapper>
                   <DetailReviewUserTagsWrapper>
                     {item.tags.map((v, idx) => {
@@ -68,68 +86,61 @@ const ReviewDetailList = () => {
                 </>
               }
               description={
-                <DetailReviewsWrapper>
-                  <DetailReviewP>교통</DetailReviewP>
-                  <Rate
-                    disabled
-                    allowHalf
-                    defaultValue={item.rate.trafficRate}
-                    style={{
-                      alignItems: "center",
-                      fontSize: "0.7rem",
-                    }}
-                  />
-                  <DetailReviewP>시설만족도</DetailReviewP>
-                  <Rate
-                    disabled
-                    allowHalf
-                    defaultValue={item.rate.facilityRate}
-                    style={{
-                      alignItems: "center",
-                      fontSize: "0.7rem",
-                    }}
-                  />
-                  <DetailReviewP>급식</DetailReviewP>
-                  <Rate
-                    disabled
-                    allowHalf
-                    defaultValue={item.rate.cafeteriaRate}
-                    style={{
-                      alignItems: "center",
-                      fontSize: "0.7rem",
-                    }}
-                  />
-                  <DetailReviewP>수업만족도</DetailReviewP>
-                  <Rate
-                    disabled
-                    allowHalf
-                    defaultValue={item.rate.educationRate}
-                    style={{
-                      alignItems: "center",
-                      fontSize: "0.7rem",
-                    }}
-                  />
-                  <DetailReviewP>취업</DetailReviewP>
-                  <Rate
-                    disabled
-                    allowHalf
-                    defaultValue={item.rate.employmentRate}
-                    style={{
-                      alignItems: "center",
-                      fontSize: "0.7rem",
-                    }}
-                  />
-                </DetailReviewsWrapper>
-              }
-            />
-            <List.Item.Meta
-              style={{
-                textAlign: "left",
-              }}
-              description={
-                <DetailReviewContentWrapper>
-                  {item.content}
-                </DetailReviewContentWrapper>
+                <>
+                  <DetailReviewsWrapper>
+                    <DetailReviewP>교통</DetailReviewP>
+                    <Rate
+                      disabled
+                      allowHalf
+                      defaultValue={item.rate.trafficRate}
+                      style={{
+                        alignItems: "center",
+                        fontSize: "0.7rem",
+                      }}
+                    />
+                    <DetailReviewP>시설만족도</DetailReviewP>
+                    <Rate
+                      disabled
+                      allowHalf
+                      defaultValue={item.rate.facilityRate}
+                      style={{
+                        alignItems: "center",
+                        fontSize: "0.7rem",
+                      }}
+                    />
+                    <DetailReviewP>급식</DetailReviewP>
+                    <Rate
+                      disabled
+                      allowHalf
+                      defaultValue={item.rate.cafeteriaRate}
+                      style={{
+                        alignItems: "center",
+                        fontSize: "0.7rem",
+                      }}
+                    />
+                    <DetailReviewP>수업만족도</DetailReviewP>
+                    <Rate
+                      disabled
+                      allowHalf
+                      defaultValue={item.rate.educationRate}
+                      style={{
+                        alignItems: "center",
+                        fontSize: "0.7rem",
+                      }}
+                    />
+                    <DetailReviewP>취업</DetailReviewP>
+                    <Rate
+                      disabled
+                      allowHalf
+                      defaultValue={item.rate.employmentRate}
+                      style={{
+                        alignItems: "center",
+                        fontSize: "0.7rem",
+                      }}
+                    />
+                  </DetailReviewsWrapper>
+                  <DetailReviewContentWrapper>{item.content}</DetailReviewContentWrapper>
+                </>
               }
             />
           </List.Item>
