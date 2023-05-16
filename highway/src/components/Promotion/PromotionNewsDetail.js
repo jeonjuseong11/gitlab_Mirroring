1ed8e76d7122b3b6ee9dff5c0927e97bf0c3aa9b
WePung <117412075+WePung@users.noreply.github.com>
2023-05-16 16:10:04 +0900

    Feat : PromotionNewsDetail.js
    버튼에 disable기능 추가

--- highway/src/components/Promotion/PromotionNewsDetail.js
+++ highway/src/components/Promotion/PromotionNewsDetail.js
@@ -4,10 +4,26 @@ import newsList from "../../utils/NewsDummyData";
 import { Button, Image } from "antd";
 
 const PromotionNewsDetail = () => {
+  const [minDisable, setMinDisable] = useState(false);
+  const [maxDisable, setMaxDisable] = useState(false);
+
+  const onChangeMaxDisable = () => {};
   // news테이블이 없기에 임시방편으로 세워둠
   const { newsId } = useParams();
-  const [id, setId] = useState(parseInt(newsId));
+  const id = parseInt(newsId);
 
+  useEffect(() => {
+    if (id === 0) {
+      setMinDisable(true);
+    } else if (id >= 1) {
+      setMinDisable(false);
+    }
+    if (id === newsList.length - 1) {
+      setMaxDisable(true);
+    } else if (id <= newsList.length - 1) {
+      setMaxDisable(false);
+    }
+  }, [id]);
   return (
     <div>
       <div>
@@ -24,13 +40,13 @@ const PromotionNewsDetail = () => {
       </div>
       <div>
         <Link to={`http://localhost:3000/promotion/news/${id - 1}`}>
-          <Button>이전</Button>
+          <button disabled={minDisable}>이전</button>
         </Link>
-        <Link to={`http://localhost:3000/promotion/news/`}>
-          <Button>목록으로</Button>
+        <Link to={`http://localhost:3000/promotion/`}>
+          <button>목록으로</button>
         </Link>
         <Link to={`http://localhost:3000/promotion/news/${id + 1}`}>
-          <Button>다음</Button>
+          <button disabled={maxDisable}>다음</button>
         </Link>
       </div>
     </div>
