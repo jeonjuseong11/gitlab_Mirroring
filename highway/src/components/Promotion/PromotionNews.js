57a8a00177f9d40221b84eed3d31755b0e5ba1ff
WePung <117412075+WePung@users.noreply.github.com>
2023-05-16 16:11:24 +0900

    Feat : PromotionNews.js
    일정 List만 보이고 더 보기 버튼 추가

--- highway/src/components/Promotion/PromotionNews.js
+++ highway/src/components/Promotion/PromotionNews.js
@@ -1,29 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AppLayout from "../AppLayout";
 import newsList from "../../utils/NewsDummyData";
 import { Link } from "react-router-dom";
-import { Image, List } from "antd";
+import { Button, Image, List } from "antd";
 
 const PromotionNews = () => {
+  const [count, setCount] = useState(3);
+  const [disable, setDisable] = useState(false);
+  const onMore = () => {
+    setCount(count + 3);
+  };
+  useEffect(() => {
+    if (count >= newsList.length) {
+      setDisable(true);
+    } else {
+      setDisable(false);
+    }
+  }, [count]);
   return (
     <AppLayout>
       <List
         itemLayout="horizontal"
         dataSource={newsList}
-        renderItem={(item) => (
-          <List.Item>
-            <Image preview={false} width={277} height={122} src={item.src} />
-            <List.Item.Meta
-              title={
-                <Link to={`/promotion/news/${item.newsId}`}>
-                  {item.newsTitle}
-                </Link>
-              }
-              description={item.newsContent}
-            />
-          </List.Item>
-        )}
+        renderItem={(item) => {
+          if (item.newsId < count)
+            return (
+              <List.Item>
+                <Image
+                  preview={false}
+                  width={277}
+                  height={122}
+                  src={item.src}
+                />
+                <List.Item.Meta
+                  title={
+                    <Link to={`/promotion/news/${item.newsId}`}>
+                      {item.newsTitle}
+                    </Link>
+                  }
+                  description={item.newsContent}
+                />
+              </List.Item>
+            );
+        }}
       />
+      <button onClick={onMore} disabled={disable}>
+        더 보기
+      </button>
     </AppLayout>
   );
 };
