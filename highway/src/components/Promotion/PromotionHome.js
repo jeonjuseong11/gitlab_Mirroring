5f082340d57d47687d0ff0dbf8ba15460a6fcaeb
WePung <117412075+WePung@users.noreply.github.com>
2023-05-16 16:12:54 +0900

    Feat : PromotionHome.js
    뉴스탭으로 가지는 더보기 버튼 추가

--- highway/src/components/Promotion/PromotionHome.js
+++ highway/src/components/Promotion/PromotionHome.js
@@ -1,15 +1,17 @@
 import React from "react";
-import AppLayout from "../AppLayout";
 import newsList from "../../utils/NewsDummyData";
 import { Link, useLocation } from "react-router-dom";
-import { Image, List } from "antd";
+import { Button, List } from "antd";
 import { NewsDummyData as items } from "../../utils/NewsDummyData";
 import {
   PromotionHomeSection,
   PromotionHomeTitleListWrapper,
   PromotionHomeWrapper,
   PromtionHomeSectionImage,
+  testUl,
+  testLi,
 } from "../../styles/PromotionStyle";
+import { RightCircleFilled } from "@ant-design/icons";
 
 const PromotionHome = () => {
   const random = Math.floor(Math.random() * items.length);
@@ -17,30 +19,39 @@ const PromotionHome = () => {
   return (
     <PromotionHomeWrapper>
       <PromotionHomeSection>
-        <h2>첫 번째 test 제목</h2>
         <div>
-          <PromtionHomeSectionImage
-            width={280}
-            height={210}
-            src={`${items[random].src}`}
-          />
+          <h2>첫 번째 test 제목</h2>
+          <Link to={`/promotion/news`}>
+            <Button icon={<RightCircleFilled />}>더보기</Button>
+          </Link>
+        </div>
+        <div>
+          <Link to={`news/${random}`}>
+            <PromtionHomeSectionImage
+              width={280}
+              height={210}
+              preview={false}
+              src={`${items[random].src}`}
+            />
+          </Link>
         </div>
         <PromotionHomeTitleListWrapper>
           <List
             itemLayout="horizontal"
             dataSource={newsList}
-            renderItem={(item) => (
-              <List.Item>
-                <List.Item.Meta
-                  title={
-                    <Link to={`/promotion/news/${item.newsId}`}>
-                      {item.newsTitle}
-                    </Link>
-                  }
-                  description={item.newsContent}
-                />
-              </List.Item>
-            )}
+            renderItem={(item) => {
+              return (
+                <List.Item>
+                  <List.Item.Meta
+                    title={
+                      <Link to={`/promotion/news/${item.newsId}`}>
+                        {item.newsTitle}
+                      </Link>
+                    }
+                  />
+                </List.Item>
+              );
+            }}
           />
         </PromotionHomeTitleListWrapper>
       </PromotionHomeSection>
