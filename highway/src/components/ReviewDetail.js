0c41fae843874302802be9f45a80f51b2d78522e
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2023-05-17 21:43:23 +0900

    Feat : ReviewLoginCheck
    로그인시에만 리뷰 작성할 수 있도록 변경

--- highway/src/components/ReviewDetail.js
+++ highway/src/components/ReviewDetail.js
@@ -1,23 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ReviewDetailWrapper } from "./SchoolDetail/SchoolDetailStyle";
 import DetailReviewForm from "./DetailReviewPost";
 import ReviewDetailList from "./ReviewDetailList";
 import { NoDecoLink } from "../styles/PageStyle";
+import { useSelector } from "react-redux";
 
 const ReviewPost = () => {
   const [write, setWrite] = useState(false);
   const onToggleWrite = () => {
     setWrite(!write);
   };
+  const { me } = useSelector((state) => state.user);
 
+  useEffect(() => {
+    if (me) {
+      console.log(me);
+    }
+  }, []);
   return (
     <>
-      <NoDecoLink
-        style={{ position: "relative", top: "1rem", left: "-1rem" }}
-        onClick={onToggleWrite}
-      >
-        {write ? "취소" : "리뷰작성"}
-      </NoDecoLink>
+      {me ? (
+        <NoDecoLink
+          style={{ position: "relative", top: "1rem", left: "-1rem" }}
+          onClick={onToggleWrite}
+        >
+          {write ? "취소" : "리뷰작성"}
+        </NoDecoLink>
+      ) : (
+        <></>
+      )}
+
       <ReviewDetailWrapper>
         {write ? <DetailReviewForm setWrite={setWrite} /> : <ReviewDetailList />}
       </ReviewDetailWrapper>
