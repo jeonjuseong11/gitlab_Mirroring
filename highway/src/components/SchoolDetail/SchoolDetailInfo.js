f0009b080121dfc330694669c2e6a5c4e4afc835
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2023-05-17 21:46:04 +0900

    Feat : SchoolDetailInfo
    학교 상세페이지 정보 탭 레이아웃 설계

--- highway/src/components/SchoolDetail/SchoolDetailInfo.js
+++ highway/src/components/SchoolDetail/SchoolDetailInfo.js
@@ -1,8 +1,49 @@
-import React from "react";
-import { SubPageWrapper } from "./SchoolDetailStyle";
+import { Col, Menu, Row, Tabs } from "antd";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { NavLink, Outlet, useParams } from "react-router-dom";
+import Tags from "../Tags";
+import { FrequentAsked, QuestionWrapper, SubPageWrapper } from "./SchoolDetailStyle";
 
 const SchoolDetailInfo = () => {
-  return <SubPageWrapper>정보</SubPageWrapper>;
+  const { schoolId, tags } = useParams();
+  const schools = useSelector((state) => state.school.schools);
+  const schoolDeparts = schools[schoolId - 1].tags;
+  const [departsList, setDepartsList] = useState([]);
+
+  return (
+    <SubPageWrapper>
+      <Row
+        gutter={[16, 16]}
+        xgutter={[16]}
+        style={{ justifyContent: "center", marginBottom: "1rem" }}
+      >
+        <Col span={4}>
+          <QuestionWrapper style={{ textAlign: "left", padding: "2rem" }}>
+            <h3>학교 정보</h3>
+            <h4>주소</h4>
+            <h4>연락처</h4>
+            <h4>계열</h4>
+            <h4>산업군</h4>
+            <h4>홈페이지</h4>
+          </QuestionWrapper>
+        </Col>
+        <Col span={10}>
+          <QuestionWrapper>
+            학과
+            {/* <Tabs items={schoolDeparts}>
+              <Tags.TabPane tab={item} key={item}></Tags.TabPane>;
+            </Tabs> */}
+          </QuestionWrapper>
+        </Col>
+      </Row>
+      <Row gutter={[16, 16]} style={{ justifyContent: "center" }}>
+        <Col span={14}>
+          <FrequentAsked>취업 / 진학</FrequentAsked>
+        </Col>
+      </Row>
+    </SubPageWrapper>
+  );
 };
 
 export default SchoolDetailInfo;
