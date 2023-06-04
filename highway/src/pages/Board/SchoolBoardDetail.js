7cbeb725d190b63396834614000ffec8f5da3cf5
WePung <akgre5@naver.com>
2023-06-05 01:36:02 +0900

    Feat : SchoolBoardComment
    임시 댓글쓰기 기능 추가

--- highway/src/pages/Board/SchoolBoardDetail.js
+++ highway/src/pages/Board/SchoolBoardDetail.js
@@ -1,8 +1,41 @@
-import { Col, Row } from "antd";
-import React from "react";
+import { Button, Col, Form, Input, Row } from "antd";
+import React, { useCallback, useEffect } from "react";
 import BoardMenu from "../../components/Board/BoardMenu";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  ADD_COMMENT_REQUEST,
+  LOAD_POST_COMMENTS_REQUEST,
+} from "../../constants/actionTypes";
+import moment from "moment";
+import { useParams } from "react-router-dom";
 
 const SchoolBoardDetail = () => {
+  const dispatch = useDispatch();
+  const { me } = useSelector((state) => state.user);
+  const { schoolBoardPostComments } = useSelector((state) => state.post);
+  const { postId } = useParams();
+  const loadPostComments = () => {
+    dispatch({
+      type: LOAD_POST_COMMENTS_REQUEST,
+      data: {
+        boardId: postId,
+      },
+    });
+  };
+
+  const handleSubmit = (values) => {
+    if (!values) {
+      alert("빈칸이 있습니다.");
+      return;
+    }
+    dispatch({
+      type: ADD_COMMENT_REQUEST,
+      data: values,
+    });
+  };
+  useEffect(() => {
+    loadPostComments();
+  }, []);
   return (
     <>
       <Row gutter={[16, 16]} justify="center">
@@ -11,7 +44,14 @@ const SchoolBoardDetail = () => {
         </Col>
       </Row>
       글 상세 페이지
-      <Row gutter={[16, 16]} justify="center"></Row>
+      <Row gutter={[16, 16]} justify="center">
+        <Form onFinish={handleSubmit}>
+          <Form.Item name="coment">
+            <Input />
+            <Button htmlType="submit" />
+          </Form.Item>
+        </Form>
+      </Row>
     </>
   );
 };
