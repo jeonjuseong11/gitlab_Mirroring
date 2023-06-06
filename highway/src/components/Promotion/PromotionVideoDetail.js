b02bfc7d07ebbe90845da07242f052e8f0f91494
WePung <akgre5@naver.com>
2023-06-07 02:16:03 +0900

    Feat : PromotionVideosPage : CSS, CTC
    PromotionVideosPage CSS수정 및 클릭시 주소복사 기능 추가

--- highway/src/components/Promotion/PromotionVideoDetail.js
+++ highway/src/components/Promotion/PromotionVideoDetail.js
@@ -1,36 +1,103 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import videoList from "../../utils/VideoDummyData"
-import { Image } from 'antd';
+import React, { useEffect, useState } from "react";
+import { Link, useLocation, useParams } from "react-router-dom";
+import videoList from "../../utils/VideoDummyData";
+import { Button, Col, Image, Row } from "antd";
+import { FieldTimeOutlined, ShareAltOutlined } from "@ant-design/icons";
+import CopyToClipboard from "react-copy-to-clipboard";
 
 const PromotionVideoDetail = () => {
+  const [minDisable, setMinDisable] = useState(false);
+  const [maxDisable, setMaxDisable] = useState(false);
 
-    const location = useLocation();
-    // video테이블이 없기에 임시방편으로 세워둠
-    const text = window.location.href;
-    const url = parseInt(text.replace("http://localhost:3000/promotion/videos/",""));
-    console.log(url)
-    const test = videoList.map((it)=>{
-        if(it.videoId === url){
-        return(
-            <div key={it.videoId}>
-                <div><video width={753} height={424} preview={false} src={it.src}/></div>
-                <div>{it.videoTitle}</div>
-                <div>{it.videoContent}</div>
+  const onChangeMaxDisable = () => {};
+  // news테이블이 없기에 임시방편으로 세워둠
+  const { videoId } = useParams();
+  const id = parseInt(videoId);
+  const location = useLocation();
+  const nowLocation = `http://localhost:3000${location.pathname}`;
+  useEffect(() => {
+    if (id === 0) {
+      setMinDisable(true);
+    } else if (id >= 1) {
+      setMinDisable(false);
+    }
+    if (id === videoList.length - 1) {
+      setMaxDisable(true);
+    } else if (id <= videoList.length - 1) {
+      setMaxDisable(false);
+    }
+  }, [id]);
+  return (
+    <>
+      <Row style={{ width: "60%", marginLeft: "20%" }}>
+        <Col style={{ width: "65%" }}>
+          <ul style={{ listStyle: "none", textAlign: "left" }}>
+            <li>
+              <h2>{videoList[id].videoTitle}</h2>
+            </li>
+            <li>
+              <p>부제목</p>
+            </li>
+            <li>
+              <p>
+                <FieldTimeOutlined style={{ marginRight: "2%" }} />
+                생성 시간
+              </p>
+            </li>
+          </ul>
+          <hr style={{ width: "100%" }} />
+          <iframe
+            width="720 "
+            height="480"
+            src={videoList[id].src}
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </Col>
+        <Col style={{ width: "35%" }}>
+          <div
+            style={{
+              width: "70%",
+              height: "500px",
+              backgroundColor: "#d2d2d2",
+              marginTop: "5%",
+              marginLeft: "10%",
+              borderRadius: "5%",
+            }}
+          >
+            우측사이드
+          </div>
+        </Col>
+      </Row>
+      <Row gutter={[16, 16]} style={{ marginLeft: "45%", marginTop: "10%" }}>
+        <Col>
+          <CopyToClipboard
+            text={nowLocation}
+            onCopy={() => alert("주소가 복사되었습니다")}
+            style={{ margin: "15%" }}
+          >
+            <div className="URL">
+              <Button>
+                <ShareAltOutlined /> 공유하기
+              </Button>
             </div>
-        )
-        }
-    })
-    return (
-        <div>
-            {test}
-            <div>
-                <Link to={`http://localhost:3000/promotion/videos/${url-1}`}><button>이전</button></Link>
-                <Link to={`http://localhost:3000/promotion/videos/`}><button>목록으로</button></Link>
-                <Link to={`http://localhost:3000/promotion/videos/${url+1}`}><button>다음</button></Link>
-            </div>
-        </div>
-    );
+          </CopyToClipboard>
+          <div>
+            <Link to={`http://localhost:3000/promotion/news/${id - 1}`}>
+              <Button disabled={minDisable}>이전</Button>
+            </Link>
+            <Link to={`http://localhost:3000/promotion/news`}>
+              <Button>목록으로</Button>
+            </Link>
+            <Link to={`http://localhost:3000/promotion/news/${id + 1}`}>
+              <Button disabled={maxDisable}>다음</Button>
+            </Link>
+          </div>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default PromotionVideoDetail;
--- highway/src/components/Promotion/PromotionVideoItem.js
+++ highway/src/components/Promotion/PromotionVideoItem.js
@@ -1,24 +1,96 @@
-import React from 'react';
-import videoList from '../../utils/VideoDummyData';
-import {Link} from 'react-router-dom';
-import { Image, List } from 'antd';
+import React, { useEffect, useState } from "react";
+import videoList from "../../utils/VideoDummyData";
+import { Link } from "react-router-dom";
+import { Image, List, Button } from "antd";
+import { DownOutlined } from "@ant-design/icons";
 
 const PromotionVideoItem = () => {
-    return (
-        <List
-        itemLayout="horizontal"
-        dataSource={videoList}
-        renderItem={(item) => (
-          <List.Item>
-            <Image preview={false} width={277} height={122} src={item.src} />
-            <List.Item.Meta
-              title={<Link to={`/promotion/videos/${item.videoId}`}>{item.videoTitle}</Link>}
-              description={item.videoContent}
-            />
-          </List.Item>
-        )}
-      />
-    );
+  const [count, setCount] = useState(3);
+  const [disable, setDisable] = useState(false);
+  const onMore = () => {
+    setCount(count + 3);
+  };
+  useEffect(() => {
+    if (count >= videoList.length) {
+      setDisable(true);
+    } else {
+      setDisable(false);
+    }
+  }, [count]);
+  return (
+    <>
+      <ul style={{ listStyle: "none", width: "100%" }}>
+        <li>
+          <List
+            itemLayout="horizontal"
+            dataSource={videoList}
+            renderItem={(item) => {
+              if (item.videoId < count)
+                return (
+                  <List.Item
+                    style={{ width: "70%", padding: "3%", marginLeft: "-4%" }}
+                  >
+                    <Link to={`/promotion/videos/${item.videoId}`}>
+                      <Image
+                        preview={false}
+                        width={277}
+                        height={122}
+                        src={item.src}
+                        style={{
+                          borderRadius: "5%",
+                        }}
+                      />
+                    </Link>
+                    <List.Item.Meta
+                      title={
+                        <div
+                          style={{
+                            textAlign: "left",
+                            marginLeft: "5%",
+                          }}
+                        >
+                          <Link to={`/promotion/videos/${item.videoId}`}>
+                            <h2 style={{ color: "black" }}>
+                              {item.videoTitle}
+                            </h2>
+                          </Link>
+                        </div>
+                      }
+                      description={
+                        <div style={{ textAlign: "left", marginLeft: "5%" }}>
+                          <Link to={`/promotion/videos/${item.videoId}`}>
+                            <p style={{ color: "gray" }}>{item.writer}</p>
+                          </Link>
+                        </div>
+                      }
+                    />
+                  </List.Item>
+                );
+            }}
+          />
+        </li>
+        <li
+          style={{
+            width: "25%",
+            height: "500px",
+            backgroundColor: "#d2d2d2",
+            marginLeft: "75%",
+            marginTop: "-51%",
+            borderRadius: "5%",
+          }}
+        >
+          우측사이드
+        </li>
+      </ul>
+      <Button
+        onClick={onMore}
+        disabled={disable}
+        style={{ marginBottom: "2%", marginTop: "2%" }}
+      >
+        <DownOutlined />더 보기
+      </Button>
+    </>
+  );
 };
 
 export default PromotionVideoItem;
