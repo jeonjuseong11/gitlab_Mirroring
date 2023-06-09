abc22c67f3440ffe1de6a5ccd71c36d5eec718b7
WePung <akgre5@naver.com>
2023-06-09 22:00:25 +0900

    Rename: News, VideoDummyData : Change Variables
    dummyData 변수명 변경

--- highway/src/utils/VideoDummyData.js
+++ highway/src/utils/VideoDummyData.js
@@ -1,46 +1,46 @@
 export const VideoDummyData = [
-    {
-        videoId : 1,
-        videoTitle : "특성화고·마이스터고 홍보영상",
-        writer : "서울특별시 교육청",
-        src :"https://youtu.be/0gMYzRkguWo",
-        videoViews: 132,
-    },
-    {
-        videoId : 2,
-        videoTitle : "안양 문화고 홍보영상",
-        writer : "안양 문화고",
-        src : "https://youtu.be/v0NtB8-wri4",
-        videoViews: 10,
-    },
-    {
-        videoId : 3,
-        videoTitle : "안양 예고 신입생 전형요강",
-        writer : "안양 예고",
-        src : "https://youtu.be/kt5gFf3MZT8",
-        videoViews: 15,
-    },
-    {
-        videoId : 4,
-        videoTitle : "서울 영상고 홍보영상",
-        writer : "서울 영상고",
-        src : "https://youtu.be/LhQjt5gZ5tY",
-        videoViews: 15,
-    },
-    {
-        videoId : 5,
-        videoTitle : "경기 자동차과학고 홍보영상",
-        writer : "경기 자동차과학고",
-        src : "https://youtu.be/uRnDNKIxbeI",
-        videoViews: 16,
-    },
-    {
-        videoId : 6,
-        videoTitle : "안양 공고 홍보영상",
-        writer : "안양 공고",
-        src :"https://youtu.be/Qh7vK4QXR4A",
-        videoViews: 17,
-    },
-]
+  {
+    id: 0,
+    title: "특성화고·마이스터고 홍보영상",
+    content: "서울특별시 교육청",
+    src: "https://youtu.be/0gMYzRkguWo",
+    views: 132,
+  },
+  {
+    id: 1,
+    title: "안양 문화고 홍보영상",
+    content: "안양 문화고",
+    src: "https://youtu.be/v0NtB8-wri4",
+    views: 10,
+  },
+  {
+    id: 2,
+    title: "안양 예고 신입생 전형요강",
+    content: "안양 예고",
+    src: "https://youtu.be/kt5gFf3MZT8",
+    views: 15,
+  },
+  {
+    id: 3,
+    title: "서울 영상고 홍보영상",
+    content: "서울 영상고",
+    src: "https://youtu.be/LhQjt5gZ5tY",
+    views: 15,
+  },
+  {
+    id: 4,
+    title: "경기 자동차과학고 홍보영상",
+    content: "경기 자동차과학고",
+    src: "https://youtu.be/uRnDNKIxbeI",
+    views: 16,
+  },
+  {
+    id: 5,
+    title: "안양 공고 홍보영상",
+    content: "안양 공고",
+    src: "https://youtu.be/Qh7vK4QXR4A",
+    views: 17,
+  },
+];
 
 export default VideoDummyData;
