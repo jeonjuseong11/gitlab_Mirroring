3b1f24415613a465817316a462dd18ce1823cd4d
WePung <akgre5@naver.com>
2023-06-15 03:23:26 +0900

    Add : CommentDummyData
    댓글 dummyData 추가

---
+++ highway/src/utils/CommentDummyDatas.js
@@ -0,0 +1,103 @@
+export const CommentDummyDatas = {
+  data: [
+    {
+      id: 204,
+      content: "undefined",
+      userId: "Lee",
+      parentId: null,
+      isDeleted: null,
+      children: [
+        {
+          id: 205,
+          content: "대댓글1",
+          userId: "admin",
+          parentId: 204,
+          isDeleted: null,
+          children: [],
+          createDate: "2023-06-14T00:11:42.156587",
+          modifiedDate: "2023-06-14T00:11:42.156587",
+        },
+        {
+          id: 211,
+          content: "대댓글1-2",
+          userId: "admin",
+          parentId: 204,
+          isDeleted: null,
+          children: [],
+          createDate: "2023-06-14T02:19:16.942642",
+          modifiedDate: "2023-06-14T02:19:16.942642",
+        },
+        {
+          id: 212,
+          content: "대댓글1-2",
+          userId: "admin",
+          parentId: 204,
+          isDeleted: null,
+          children: [],
+          createDate: "2023-06-14T02:36:16.797816",
+          modifiedDate: "2023-06-14T02:36:16.797816",
+        },
+      ],
+      createDate: "2023-06-14T00:11:13.905074",
+      modifiedDate: "2023-06-14T02:35:25.955324",
+    },
+    {
+      id: 206,
+      content: "undefined",
+      userId: "Lee",
+      parentId: null,
+      isDeleted: null,
+      children: [
+        {
+          id: 207,
+          content: "대댓글2",
+          userId: "admin",
+          parentId: 206,
+          isDeleted: null,
+          children: [],
+          createDate: "2023-06-14T00:21:46.499688",
+          modifiedDate: "2023-06-14T00:21:46.499688",
+        },
+      ],
+      createDate: "2023-06-14T00:20:59.959583",
+      modifiedDate: "2023-06-14T02:35:25.994333",
+    },
+    {
+      id: 208,
+      content: "undefined",
+      userId: "Lee",
+      parentId: null,
+      isDeleted: null,
+      children: [],
+      createDate: "2023-06-14T01:11:52.65487",
+      modifiedDate: "2023-06-14T02:37:23.491062",
+    },
+    {
+      id: 213,
+      content: "undefined",
+      userId: "admin",
+      parentId: null,
+      isDeleted: null,
+      children: [],
+      createDate: "2023-06-14T02:36:36.84181",
+      modifiedDate: "2023-06-14T02:37:20.425269",
+    },
+    {
+      id: 214,
+      content: "123",
+      userId: "admin",
+      parentId: null,
+      isDeleted: null,
+      children: [],
+      createDate: "2023-06-14T03:11:14.026934",
+      modifiedDate: "2023-06-14T03:11:14.026934",
+    },
+  ],
+};
+
+// export const CommentDummyDatas = {
+//   data: [
+//   ],
+// };
+
+export default CommentDummyDatas;
