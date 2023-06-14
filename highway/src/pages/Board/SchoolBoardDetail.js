2077b70f1d3993f239b53cd9707c9414dd09c77a
WePung <akgre5@naver.com>
2023-06-15 03:25:55 +0900

    Edit : SchoolBoardDetail.js : CSS
    SchoolBoardDetailPage CSS 수정

--- highway/src/pages/Board/SchoolBoardDetail.js
+++ highway/src/pages/Board/SchoolBoardDetail.js
@@ -3,9 +3,8 @@ import {
   FieldTimeOutlined,
   LikeOutlined,
 } from "@ant-design/icons";
-import { Button, Descriptions, Input, List, Row, Form } from "antd";
+import { Button, Descriptions, Input, List, Row, Form, Col } from "antd";
 import React, { useEffect, useState } from "react";
-import BoardMenu from "../../components/Board/BoardMenu";
 import { useDispatch, useSelector } from "react-redux";
 import {
   ADD_COMMENT_REQUEST,
@@ -16,14 +15,18 @@ import {
 import { useParams } from "react-router-dom";
 import BoardProfile from "../../components/Board/BoardProfile";
 import moment from "moment";
-import ToggleInput from "../../components/schoolBoardDetail/ToggleInput";
+import CommentDummyData, {
+  CommentDummyDatas,
+} from "../../utils/CommentDummyDatas";
+import SchoolBoardMenu from "../../components/schoolBoardDetail/SchoolBoardMenu";
+import ToggleComment from "../../components/schoolBoardDetail/ToggleComment";
 
 const dummyboardData = [
   {
     id: 1,
     category: 1,
     content: "테스트 내용1",
-    createData: "1시간전",
+    createData: moment,
     modifiedDate: "1시간전",
     title: "테스트1",
     userId: "admin",
@@ -35,24 +38,8 @@ const dummyboardData = [
 const SchoolBoardDetail = () => {
   const dispatch = useDispatch();
   const { me } = useSelector((state) => state.user);
-  const { schoolBoardPostComments } = useSelector((state) => state.post);
-  const schoolBoardPostCommentsData = schoolBoardPostComments.data;
-  const [replyComment, setReplyCommnet] = useState([]);
-  const { postId } = useParams();
   const [parentId, setParentId] = useState(null);
-  const [commentNum, setCommentNum] = useState(false);
-  const [commnetCount, setCommentCount] = useState(0);
-
-  const userCheck = (item) => {
-    if (me === null) {
-      return false;
-    }
-    if (me.userId === item.userId) {
-      return true;
-    } else {
-      return false;
-    }
-  };
+  const postId = 1;
   const loadPostComments = () => {
     console.log("loadPostCommnets");
     dispatch({
@@ -63,132 +50,33 @@ const SchoolBoardDetail = () => {
     });
   };
 
-  const onFinish = (values) => {
-    if (values === undefined) {
-      alert("빈칸이 있습니다.");
-      return;
-    }
-    dispatch({
-      type: ADD_COMMENT_REQUEST,
-      data: {
-        content: values.content,
-        createData: moment(),
-        modifiedDate: moment(),
-        userId: me.userId,
-        boardId: postId,
-        parentId: parentId,
-      },
-    });
-  };
-
-  useEffect(() => {
-    loadPostComments();
-  }, []);
   return (
-    <>
-      <BoardMenu />
-      <Row gutter={[16, 16]} justify="center">
+    <Row gutter={[16, 16]} justify="center" style={{ paddingTop: "1rem" }}>
+      <SchoolBoardMenu />
+      <Col xs={23} md={11} style={{ textAlign: "left" }}>
+        <h2>{dummyboardData[postId - 1].title}</h2>
         <ul
           style={{
-            width: "75rem",
             listStyle: "none",
+            marginLeft: "-2.5rem",
           }}
         >
-          <li
-            style={{
-              width: "51rem",
-              marginLeft: "24rem",
-            }}
-          >
-            <ul
-              style={{
-                listStyle: "none",
-                textAlign: "left",
-                marginLeft: "-2.5rem",
-                marginBottom: "1.5rem",
-              }}
-            >
-              <li>
-                <div>
-                  <h1>{dummyboardData[postId - 1].title}</h1>
-                </div>
-              </li>
-              <li>
-                <div>작성자 : {dummyboardData[postId - 1].userId}</div>
-              </li>
-              <li style={{ marginTop: "1rem", marginRight: "1rem" }}>
-                <FieldTimeOutlined />
-                {dummyboardData[postId - 1].createData}
-                <CommentOutlined
-                  style={{ marginLeft: "1rem", marginRight: "0.5rem" }}
-                />
-                {schoolBoardPostCommentsData.length}
-                <LikeOutlined
-                  style={{ marginLeft: "1rem", marginRight: "0.5rem" }}
-                />
-                {dummyboardData[postId - 1].good}
-              </li>
-            </ul>
-            <hr />
-            <Descriptions>
-              <Descriptions.Item>
-                {dummyboardData[postId - 1].content}
-              </Descriptions.Item>
-            </Descriptions>
-            <div
-              style={{
-                width: "51rem",
-              }}
-            >
-              <ul
-                style={{
-                  listStyle: "none",
-                  width: "51rem",
-                  marginTop: "2.5rem",
-                  marginBottom: "1.5rem",
-                  marginLeft: "-2.5rem",
-                }}
-              >
-                <Form onFinish={onFinish}>
-                  <li style={{ float: "left", width: "42rem" }}>
-                    <Form.Item name="content">
-                      <Input
-                        rows={3}
-                        placeholder="댓글을 적어주세요."
-                        style={{
-                          width: "40rem",
-                          height: "5rem",
-                          marginLeft: "-2rem",
-                        }}
-                      />
-                    </Form.Item>
-                  </li>
-                  <li style={{ width: "51rem" }}>
-                    <Form.Item>
-                      <Button
-                        type="primary"
-                        htmlType="submit"
-                        style={{
-                          width: "9rem",
-                          marginLeft: "-0.5rem",
-                          height: "5rem",
-                        }}
-                      >
-                        등록
-                      </Button>
-                    </Form.Item>
-                  </li>
-                </Form>
-              </ul>
-              <ToggleInput />
-            </div>
-            <li style={{ marginTop: "5%" }}>
-              <Button>더 보기</Button>
-            </li>
+          <li style={{ marginBottom: "0.5rem" }}>
+            작성자 : {dummyboardData[postId - 1].userId}
           </li>
+          <li style={{ float: "left" }}>
+            {dummyboardData[postId - 1].createData}
+          </li>
+          <li style={{ float: "left" }}>{CommentDummyData.length}</li>
+          <li style={{ marginBottom: "2.5rem" }}>
+            {dummyboardData[postId - 1].good}
+          </li>
+          <hr />
+          {dummyboardData[postId - 1].content}
         </ul>
-      </Row>
-    </>
+      </Col>
+      <ToggleComment />
+    </Row>
   );
 };
 export default SchoolBoardDetail;
