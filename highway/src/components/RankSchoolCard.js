90ecf94995f44f16b338701060e9f311e114a66b
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2023-06-26 21:44:54 +0900

    Edit: SchoolDetailLink
    랭킹페이지 링크 수정

--- highway/src/components/RankSchoolCard.js
+++ highway/src/components/RankSchoolCard.js
@@ -16,7 +16,7 @@ const RankSchoolCard = ({ selectedSchool }) => {
     <Card
       style={{ backgroundColor: "#f2f2f2", minWidth: "18rem" }}
       actions={[
-        <Link to={`/schooldetail/${selectedSchool.id}/review/`}>
+        <Link to={`/schooldetail/${selectedSchool.id}`}>
           <SearchOutlined /> 상세 정보 보기
         </Link>,
       ]}
