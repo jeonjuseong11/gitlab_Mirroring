3fe90a439a51dea24c7601295e2ca30ba12a610c
WePung <akgre5@naver.com>
2023-07-13 00:32:55 +0900

    Edit : Term : Modal
    이용약관 Modal화 (진행중, 미완성)

--- highway/src/pages/Terms.js
+++ highway/src/pages/Terms.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { TermWrapper } from "../styles/TermsStyle";
 import Service from "../components/Terms/Service";
 import Private from "../components/Terms/Private";
 import YoungPrivate from "../components/Terms/YoungPrivate";
 import TermBtn from "../components/Terms/TermBtn";
-import { Col, Row } from "antd";
+import { Col, Modal, Row } from "antd";
+import TermsContents from "../utils/TermsContents";
 
 const Terms = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <TermWrapper>
+      <Row justify="center">
+        <Col xs={12} md={9} style={{ border: "1px solid blakc" }}>
+          <p style={{ color: "black" }}>이용약관(필수)</p>
+          <TermsContents setOpen={setOpen} />
+        </Col>
+      </Row>
       <Row justify="center">
         <Col xs={24} md={18}>
           <Service />
