49f2aad8aae20e6932b77b78009d047bf3a6f7f9
WePung <akgre5@naver.com>
2023-07-15 22:24:02 +0900

    Edit : BoardDetailUpdateForm : CSS
    CSS 수정

--- highway/src/components/Board/BoardDetailUptadeForm.js
+++ highway/src/components/Board/BoardDetailUptadeForm.js
@@ -1,37 +1,47 @@
 import React, { useCallback, useEffect, useState } from "react";
-import {
-  Form,
-  Input,
-  Button,
-  Upload,
-  Modal,
-  Row,
-  Col,
-  Select,
-  message,
-} from "antd";
-import { EditOutlined, PlusOutlined } from "@ant-design/icons";
-import {
-  ADD_POST_REQUEST,
-  UPDATE_POST_REQUEST,
-} from "../../constants/actionTypes";
+import { Form, Input, Button, Row, Col, Select, message } from "antd";
+import { EditOutlined } from "@ant-design/icons";
+import { UPDATE_POST_REQUEST } from "../../constants/actionTypes";
 import { useNavigate, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import { changeCategory } from "../../pages/Board/BoardMain";
+import ReactQuill from "react-quill";
+import styled from "styled-components";
 
 const { TextArea } = Input;
 
+const CustomQuillWrapper = styled(Form.Item)`
+  .ql-container {
+    border-color: ${(props) =>
+      props.isFocused ? "#a8a8fe !important" : "#f2f2f2 !important"};
+    border-radius: 0 0 10px 10px;
+  }
+
+  .ql-toolbar {
+    border-color: ${(props) =>
+      props.isFocused ? "#a8a8fe !important" : "#f2f2f2 !important"};
+    border-radius: 10px 10px 0 0;
+  }
+  .ql-container::placeholder {
+    color: #c2c2c2;
+    font-weight: 700;
+  }
+  .ql-container:focus::placeholder {
+    color: #a8a8fe;
+  }
+`;
+
 const BoardDetailUptadeForm = () => {
+  const { schoolBoardPost } = useSelector((state) => state.post);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { addPostLoading } = useSelector((state) => state.post);
   const [form] = Form.useForm();
-  const [previewVisible, setPreviewVisible] = useState(false);
-  const [previewImage, setPreviewImage] = useState("");
   const [fileList, setFileList] = useState([]);
   const { me } = useSelector((state) => state.user);
   const accessToken = localStorage.getItem("ACCESSTOKEN");
-  const { schoolBoardPost } = useSelector((state) => state.post);
-  console.log(schoolBoardPost);
+  const [isEditorFocused, setIsEditorFocused] = useState(false);
+  const [content, setContent] = useState("");
+  const navigator = useNavigate();
 
   useEffect(() => {
     // 새로고침 시 로컬 스토리지에서 로그인 정보 가져오기
@@ -41,29 +51,7 @@ const BoardDetailUptadeForm = () => {
     }
   }, [navigate, accessToken]);
   const { postId } = useParams();
-  const boardPost = useCallback(
-    (values) => {
-      const schoolId = values.category == "10" ? 0 : me?.schoolId;
-      dispatch({
-        type: ADD_POST_REQUEST,
-        data: {
-          title: values.title,
-          content: values.content.replace(/\r?\n/g, "<br>"),
-          category: values.category,
-          schoolId: schoolId,
-        },
-      });
-      navigateToHomeBoard(values.category);
-    },
-    [dispatch, me]
-  );
 
-  const navigateToHomeBoard = useCallback(
-    (category) => {
-      navigate(`/schoolboard/${category}`);
-    },
-    [navigate]
-  );
   const onFinish = (values) => {
     dispatch({
       type: UPDATE_POST_REQUEST,
@@ -74,30 +62,12 @@ const BoardDetailUptadeForm = () => {
         id: postId,
       },
     });
-    // navigate(`/schoolboard/${values.category}`);
     window.location.replace(`/schoolboard/${values.category}`);
   };
 
   const handleChange = ({ fileList }) => {
     setFileList(fileList);
   };
-
-  const handleCancel = () => {
-    setPreviewVisible(false);
-  };
-
-  const beforeUpload = (file) => {
-    const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
-    if (!isJpgOrPng) {
-      message.error("JPG/PNG 형식의 이미지 파일만 업로드 가능합니다!");
-    }
-    const isLt2M = file.size / 1024 / 1024 < 2;
-    if (!isLt2M) {
-      message.error("이미지 파일 크기는 2MB보다 작아야 합니다!");
-    }
-    return isJpgOrPng && isLt2M;
-  };
-
   const [options, setOptions] = useState();
   useEffect(() => {
     if (me?.tag) {
@@ -116,17 +86,13 @@ const BoardDetailUptadeForm = () => {
     <Row gutter={[16, 16]} justify="center">
       <Col
         xs={24}
-        md={21}
-        style={{
-          textAlign: "left",
-          marginTop: "1rem",
-          padding: "1rem",
-        }}
+        md={15}
+        style={{ textAlign: "left", marginTop: "1rem", padding: "1rem" }}
       >
         <Form form={form} onFinish={onFinish}>
           <Form.Item name="category">
             <Select
-              placeholder="게시판 종류"
+              placeholder={changeCategory(schoolBoardPost?.board.category)}
               style={{
                 width: "30%",
                 borderRadius: "0",
@@ -135,54 +101,74 @@ const BoardDetailUptadeForm = () => {
             />
           </Form.Item>
           <Form.Item name="title">
-            <Input className="custom-input" placeholder="제목을 입력해주세요" />
-          </Form.Item>
-          <Form.Item name="content">
-            <TextArea
-              rows={10}
-              style={{ resize: "none", padding: "1rem" }}
-              placeholder="
-                글의 내용을 입력해주세요 &#13;&#10;주제에 맞지 않는 글이나 커뮤니티 이용정책에 위배되어 일정 수
-                이상 신고를 받는 경우 글이 블라인드 처리될 수 있습니다.
-              "
+            <Input
+              className="custom-input"
+              placeholder={`${schoolBoardPost?.board.title}`}
             />
           </Form.Item>
-          {/* <Form.Item name="photo">
-            <label>사진</label>
-            <Upload
-              beforeUpload={beforeUpload}
-              fileList={fileList}
+          <CustomQuillWrapper isFocused={isEditorFocused} name="content">
+            <ReactQuill
+              placeholder={`${schoolBoardPost?.board.content}`}
+              value={content}
+              style={{ height: "20rem" }}
               onChange={handleChange}
-              onPreview={handlePreview}
-              listType="picture-card"
-            >
-              {fileList.length >= 8 ? null : (
-                <div>
-                  <PlusOutlined />
-                  <div style={{ marginTop: 8 }}>Upload</div>
-                </div>
-              )}
-            </Upload>
-          </Form.Item> */}
+              theme="snow"
+              onFocus={() => setIsEditorFocused(true)}
+              onBlur={() => setIsEditorFocused(false)}
+              modules={{
+                toolbar: [
+                  [{ header: [1, 2, 3, 4, 5, 6] }, { font: [] }],
+                  ["bold", "italic", "underline", "strike", "blockquote"],
+                  [
+                    { list: "ordered" },
+                    { list: "bullet" },
+                    { indent: "-1" },
+                    { indent: "+1" },
+                  ],
+                  ["link", "image", "video"],
+                  [{ direction: "rtl" }],
+                  [{ color: [] }, { background: [] }],
+                  [{ align: [] }],
+                  ["clean"],
+                ],
+                clipboard: {
+                  // toggle to add extra line breaks when pasting HTML:
+                  matchVisual: false,
+                },
+              }}
+            />
+          </CustomQuillWrapper>
           <Form.Item>
-            {/* <a href={`/schoolboard/`}> */}
             <Button
-              type="primary"
-              htmlType="submit"
+              onClick={() => {
+                // navigator(`/schoolboard/${values.category}`) // 카테고리로 돌아갈려면
+                navigator(-1);
+              }}
               style={{
                 width: "10rem",
+                marginTop: "2rem",
+                marginLeft: "1rem",
                 height: "3rem",
                 borderRadius: "50px",
                 float: "right",
               }}
             >
-              수정 <EditOutlined />
+              취소
             </Button>
-            {/* </a> */}
+            <Button
+              type="primary"
+              htmlType="submit"
+              style={{
+                width: "10rem",
+                marginTop: "2rem",
+                height: "3rem",
+                borderRadius: "50px",
+                float: "right",
+              }}
+            >
+              수정하기 <EditOutlined />
+            </Button>
           </Form.Item>
-          <Modal visible={previewVisible} footer={null} onCancel={handleCancel}>
-            <img alt="Preview" style={{ width: "100%" }} src={previewImage} />
-          </Modal>
         </Form>
       </Col>
     </Row>
