7db441888814245e63110ca6262467bf7846abb0
WePung <akgre5@naver.com>
2023-07-20 15:44:01 +0900

    Edit : SchoolBoardPage
    me정보가 없을 시 잼민(임시)게시판으로 이동 및 다른 게시판은 로그인 시 가능다는 modal창 출력

--- highway/src/pages/SchoolBoard.js
+++ highway/src/pages/SchoolBoard.js
@@ -1,6 +1,11 @@
 import { Button, Col, Menu, Row } from "antd";
 import { Outlet, useLocation, useNavigate, useParams } from "react-router-dom";
-import { EditOutlined, SearchOutlined, SmileOutlined, TeamOutlined } from "@ant-design/icons";
+import {
+  EditOutlined,
+  SearchOutlined,
+  SmileOutlined,
+  TeamOutlined,
+} from "@ant-design/icons";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { needLoginError } from "../utils/Message";
@@ -11,6 +16,12 @@ const SchoolBoard = () => {
   const [title, setTItle] = useState("커뮤니티");
   const { me } = useSelector((state) => state.user);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!me) {
+      navigate("/schoolboard/10");
+    }
+  }, []);
 
   useEffect(() => {
     if (category == "0") {
@@ -30,19 +41,37 @@ const SchoolBoard = () => {
       key: "/schoolboard/0",
       icon: <SmileOutlined />,
       label: "자유게시판",
-      onClick: () => navigate("/schoolboard/0"),
+      onClick: () => {
+        if (!me) {
+          needLoginError("글쓰기는 로그인 후에 가능합니다.", navigate);
+        } else {
+          navigate("/schoolboard/0");
+        }
+      },
     },
     {
       key: "/schoolboard/1",
       icon: <SearchOutlined />,
       label: "질문게시판",
-      onClick: () => navigate("/schoolboard/1"),
+      onClick: () => {
+        if (!me) {
+          needLoginError("글쓰기는 로그인 후에 가능합니다.", navigate);
+        } else {
+          navigate("/schoolboard/1");
+        }
+      },
     },
     {
       key: "/schoolboard/2",
       icon: <TeamOutlined />,
       label: "프로젝트 모집",
-      onClick: () => navigate("/schoolboard/2"),
+      onClick: () => {
+        if (!me) {
+          needLoginError("글쓰기는 로그인 후에 가능합니다.", navigate);
+        } else {
+          navigate("/schoolboard/2");
+        }
+      },
     },
     {
       key: "/schoolboard/10",
@@ -63,7 +92,13 @@ const SchoolBoard = () => {
     <div>
       <Row gutter={[16, 16]} justify="center" style={{ paddingTop: "1rem" }}>
         <Col xs={24} md={15}>
-          <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
             <h1 style={{ margin: "0", marginLeft: "1rem" }}>{title}</h1>
             {/* <Link to="/schoolboard/post"> */}
             <Button
