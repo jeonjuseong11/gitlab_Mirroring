23272d6bcce47292195197599031526799c1bdc2
jeonjuseong11 <jeonjuseong11@gmail.com>
2023-07-31 00:07:49 +0900

    Edit : Menu
    모바일 화면 시 메뉴 가운데 배치

--- highway/src/components/Menu/TopMenu.js
+++ highway/src/components/Menu/TopMenu.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import Header from "../Header";
 import { Col, Menu, Row } from "antd";
@@ -12,13 +12,42 @@ const TopMenu = () => {
   }, [location]);
   const selectedKey = getSelectedKey();
 
+  // State to hold the window width
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  // Function to update window width state on window resize
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  // Add event listener for window resize
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  // Determine if it's an xs screen (screen width less than or equal to 576px)
+  const isXsScreen = windowWidth <= 576;
+
   return (
     <div>
       <Header />
       <MenuWrapper>
         <Row justify="center" gutter={[16, 16]}>
           <Col xs={24} md={15}>
-            <Menu mode="horizontal" items={MenuLists} selectedKeys={[selectedKey]} />
+            <Menu
+              style={{
+                fontSize: "1.1rem",
+                justifyContent: isXsScreen ? "center" : "inherit",
+              }}
+              mode="horizontal"
+              items={MenuLists}
+              selectedKeys={[selectedKey]}
+            />
           </Col>
         </Row>
       </MenuWrapper>
