093a3febeb615f39bfd8b23df7179733df61b893
WePung <akgre5@naver.com>
2023-08-18 01:34:48 +0900

    Edit : PromotionSlider : CSS
    버튼 CSS 수정

--- highway/src/components/Promotion/PromotionSilder.js
+++ highway/src/components/Promotion/PromotionSilder.js
@@ -8,46 +8,60 @@ import { VideoDummyData as videosItems } from "../../utils/VideoDummyData";
 //   const randomTwo = Math.floor(Math.random() * infoData.length);
 //   const randomThree = Math.floor(Math.random() * infoData.length);
 const PromotionSilder = () => {
-  const [randomOne, setRandomOne] = useState(
-    Math.floor(Math.random() * videosItems.length)
-  );
-  const [randomTwo, setRandomTwo] = useState(
-    Math.floor(Math.random() * videosItems.length)
-  );
-  const [randomThree, setRandomThree] = useState(
+  // const [randomOne, setRandomOne] = useState(
+  //   Math.floor(Math.random() * videosItems.length)
+  // );
+  // const [randomTwo, setRandomTwo] = useState(
+  //   Math.floor(Math.random() * videosItems.length)
+  // );
+  // const [randomThree, setRandomThree] = useState(
+  //   Math.floor(Math.random() * videosItems.length)
+  // );
+  // const prevSlide = () => {
+  //   setRandomOne(randomTwo);
+  //   setRandomTwo(randomThree);
+  //   setRandomThree(randomThree - 1);
+  // };
+  // const nextSlide = () => {
+  //   setRandomOne(randomOne + 1);
+  //   setRandomTwo(randomOne);
+  //   setRandomThree(randomTwo);
+  // };
+  const [random, setRandom] = useState(
     Math.floor(Math.random() * videosItems.length)
   );
   const prevSlide = () => {
-    setRandomOne(randomTwo);
-    setRandomTwo(randomThree);
-    setRandomThree(randomThree - 1);
+    setRandom(random - 1);
   };
   const nextSlide = () => {
-    setRandomOne(randomOne + 1);
-    setRandomTwo(randomOne);
-    setRandomThree(randomTwo);
+    setRandom(random + 1);
   };
   return (
     <>
       <Row justify="center">
         <LeftOutlined
+          style={{ backgroundColor: "rgb(220, 220, 225)" }}
           onClick={() => {
             prevSlide();
           }}
         />
         <Col md={4} style={{ backgroundColor: "red" }}>
-          <img src={videosItems[randomOne].image} style={{ width: "18rem" }} />
+          <img
+            src={videosItems[random - 1]?.image}
+            style={{ width: "18rem" }}
+          />
         </Col>
         <Col md={4} style={{ backgroundColor: "green" }}>
-          <img src={videosItems[randomTwo].image} style={{ width: "18rem" }} />
+          <img src={videosItems[random]?.image} style={{ width: "18rem" }} />
         </Col>
         <Col md={4} style={{ backgroundColor: "orange" }}>
           <img
-            src={videosItems[randomThree].image}
+            src={videosItems[random + 1]?.image}
             style={{ width: "18rem" }}
           />
         </Col>
         <RightOutlined
+          style={{ backgroundColor: "rgb(220, 220, 225)" }}
           onClick={() => {
             nextSlide();
           }}
