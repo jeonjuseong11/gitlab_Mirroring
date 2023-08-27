498e1e47249d1cda45b6f4ed8dcf5224faedab7d
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2023-08-27 22:40:32 +0900

    ADD : Banner_small
    작은 버전의 이미지 추가

--- highway/src/components/Slider/AdSlider.js
+++ highway/src/components/Slider/AdSlider.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./AdSlider.css";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-const banner1 = `/assets/Banner1.png`;
-const banner2 = `/assets/Banner2.png`;
-const banner3 = `/assets/Banner3.png`;
-const banner4 = `/assets/Banner4.png`;
+const banner1 = {
+  small: `/assets/Banner1_small.png`,
+  large: `/assets/Banner1.png`,
+};
+
+const banner2 = {
+  small: `/assets/Banner2_small.png`,
+  large: `/assets/Banner2.png`,
+};
+
+const banner3 = {
+  small: `/assets/Banner3_small.png`,
+  large: `/assets/Banner3.png`,
+};
+
+const banner4 = {
+  small: `/assets/Banner4_small.png`,
+  large: `/assets/Banner4.png`,
+};
 const SlideItem = styled.div`
   border-radius: 5px;
   overflow: hidden;
@@ -45,27 +60,52 @@ function AdSlider() {
       slides[next].classList.add("active");
     },
   };
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <div>
       <Slider {...settings}>
         <Link to={`/schoolRanking`}>
           <SlideItem backgroundColor="#f5efd0">
-            <img src={banner1} style={{ height: "100%", margin: "0 auto" }} />
+            <img
+              src={windowWidth <= 1200 ? banner1.small : banner1.large}
+              style={{ height: "100%", margin: "0 auto" }}
+            />
           </SlideItem>
         </Link>
         <Link to={`/schoolboard/0`}>
           <SlideItem backgroundColor="#d0e3f7" secondBackgroundColor="#d0e3f7">
-            <img src={banner2} style={{ height: "100%", margin: "0 auto" }} />
+            <img
+              src={windowWidth <= 1200 ? banner2.small : banner2.large}
+              style={{ height: "100%", margin: "0 auto" }}
+            />
           </SlideItem>
         </Link>
         <Link to={`/schoolboard/0`}>
           <SlideItem backgroundColor="#a6d8ba" secondBackgroundColor="#81cc9f">
-            <img src={banner3} style={{ height: "100%", margin: "0 auto" }} />
+            <img
+              src={windowWidth <= 1200 ? banner3.small : banner3.large}
+              style={{ height: "100%", margin: "0 auto" }}
+            />
           </SlideItem>
         </Link>
         <Link to={`/schoolboard/0`}>
           <SlideItem backgroundColor="#d1c4db" secondBackgroundColor="#ba8bc7">
-            <img src={banner4} style={{ height: "100%", margin: "0 auto" }} />
+            <img
+              src={windowWidth <= 1200 ? banner4.small : banner4.large}
+              style={{ height: "100%", margin: "0 auto" }}
+            />
           </SlideItem>
         </Link>
       </Slider>
