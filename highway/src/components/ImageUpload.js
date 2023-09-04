2b4ae22b01e9d06e1efcf4b04d46be787d2c5fbf
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2023-09-04 12:44:18 +0900

    Refactor : ImageUpload
    업로드시 업로드 목록에서 이미지 미리보기 기능 추가

--- highway/src/components/ImageUpload.js
+++ highway/src/components/ImageUpload.js
@@ -7,6 +7,8 @@ import { REMOVE_IMAGE, UPLOAD_IMAGES_REQUEST } from "../constants/actionTypes";
 const ImageUpload = () => {
   const [fileList, setFileList] = useState([]);
   const [modalVisible, setModalVisible] = useState(false);
+  const [previewVisible, setPreviewVisible] = useState(false);
+  const [previewImage, setPreviewImage] = useState("");
   const dispatch = useDispatch();
   const uploadImagesLoading = useSelector((state) => state.post.uploadImagesLoading);
   const { imagePaths } = useSelector((state) => state.post);
@@ -15,12 +17,19 @@ const ImageUpload = () => {
     setModalVisible(false);
   };
 
+  const handlePreview = (file) => {
+    const index = fileList.indexOf(file);
+    if (index !== -1) {
+      setPreviewImage(imagePaths[index]);
+      setPreviewVisible(true);
+    }
+  };
+
   useEffect(() => {
     console.log(uploadImagesLoading);
   }, [uploadImagesLoading]);
 
   const handleFileChange = (file) => {
-    // 이미지 파일만 필터링하여 업로드합니다.
     console.log(file);
     const isSizeValid = file.size <= 10 * 1024 * 1024; // 10MB
 
@@ -39,19 +48,22 @@ const ImageUpload = () => {
     });
 
     // 이미지를 fileList에 추가합니다.
-    setFileList([...fileList, file]);
+    const newFile = { ...file };
+    newFile.url = URL.createObjectURL(file); // 이미지 파일의 url 속성 설정
+    setFileList([...fileList, newFile]); // 수정된 파일 객체를 추가
   };
 
   const handleRemove = (file) => {
-    // 이미지를 제거하는 액션을 디스패치합니다.
-    dispatch({
-      type: REMOVE_IMAGE,
-      data: fileList.indexOf(file),
-    });
+    const index = fileList.indexOf(file);
+    if (index !== -1) {
+      dispatch({
+        type: REMOVE_IMAGE,
+        data: index,
+      });
 
-    // 이미지를 fileList에서 제거합니다.
-    const newFileList = fileList.filter((item) => item !== file);
-    setFileList(newFileList);
+      const newFileList = fileList.filter((item) => item !== file);
+      setFileList(newFileList);
+    }
   };
 
   useEffect(() => {
@@ -73,8 +85,9 @@ const ImageUpload = () => {
         fileList={fileList}
         multiple
         listType="picture-card"
-        customRequest={({ file }) => handleFileChange(file)} // 파일 하나로 수정
+        customRequest={({ file }) => handleFileChange(file)}
         onRemove={handleRemove}
+        onPreview={handlePreview}
         beforeUpload={beforeUploadCheck}
       >
         {fileList?.length >= 8 ? null : (
@@ -84,8 +97,18 @@ const ImageUpload = () => {
           </div>
         )}
       </Upload>
+
       <Modal visible={modalVisible} onCancel={handleCloseModal} onOk={handleCloseModal} centered>
         <p>이미지 파일은 10MB 이하만 업로드 가능합니다.</p>
+      </Modal>
+
+      <Modal
+        visible={previewVisible}
+        onCancel={() => setPreviewVisible(false)}
+        centered
+        footer={null}
+      >
+        <img alt="Preview" src={previewImage} style={{ width: "100%" }} />
       </Modal>
     </>
   );
