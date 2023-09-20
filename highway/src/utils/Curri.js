132d28de1a5e77621d7367277ca5b86611556d26
WePung <akgre5@naver.com>
2023-09-21 00:40:34 +0900

    Add : Curri.js
    학과 더미 데이터 추가

---
+++ highway/src/utils/Curri.js
@@ -0,0 +1,536 @@
+export const descCurri = [
+  {
+    schoolId: 2,
+    desc: [
+      {
+        descTitle: "패션 디자인",
+        grade: [
+          {
+            grade: 1,
+            depart: [
+              {
+                departTitle: "미용의 기초",
+                choose: "Public",
+                chapter: [
+                  {
+                    step1: " 미용의 의의와 발달",
+                    content: "미용의 의의, 미용의 변천, 미용인의 자세와 진로",
+                  },
+                  {
+                    step2: "모발의 이해",
+                    content:
+                      "모발의 생성과 성장, 모발의 구조와 특성, 모발의 분류",
+                  },
+                  {
+                    step3: "피부의 이해",
+                    content:
+                      "피부의 구조와 생리적 기능, 피부 부속 기관의 구조와 생리적 기능, 근육과 골격 및 혈관 림프계",
+                  },
+                  {
+                    step4: "메이크업과 색채",
+                    content: "피부색과 색상의 조화, 메이크업과 색채 활용",
+                  },
+                  {
+                    step5: "손발톱의 이해",
+                    content:
+                      "손톱과 발톱의 구조 및 생리적 특성, 손톱과 발톱 관리",
+                  },
+                  {
+                    step6: "고객 상담",
+                    content: "고객 응대하기, 고객 상담하기",
+                  },
+                ],
+              },
+              {
+                departTitle: "컴퓨터 시스템 일반",
+                choose: "Public",
+                chapter: [
+                  {
+                    step1: "정보의 표현",
+                    content:
+                      "수의 체계, 디지털 정보의 연산, 디지털 정보의 표현",
+                  },
+                  {
+                    step2: "컴퓨팅 시스템",
+                    content:
+                      "컴퓨팅 시스템의 구성과 동작, 논리 연산과 논리 회로, 네트워크의 이해",
+                  },
+                  {
+                    step3: "운영 체제의 자원 관리",
+                    content:
+                      "운영 체제의 구성, 프로세스 관리, 주기억 장치 관리, 보조 기억 장치 관리, 가상 기억 장치 관리, 입출력과 파일 관리",
+                  },
+                  {
+                    step4: "운영 체제 운용",
+                    content:
+                      "리눅스의 이해, 기본 명령어, 사용자 관리와 파일 속성, 리눅스 관리자 명령어, 네트워크 설정 명령어",
+                  },
+                  {
+                    step4: "문제 해결과 알고리즘",
+                    content: "추상화, 알고리즘 설계",
+                  },
+                ],
+              },
+              {
+                departTitle: "프로그래밍",
+                choose: "Public",
+                chapter: [
+                  {
+                    step1: "프로그래밍의 개요",
+                    content:
+                      "프로그래밍 언어의 종류와 특징, 프로그래밍의 절차, 알고리즘, 프로그래밍 개발 환경",
+                  },
+                  {
+                    step2: "프로그래밍의 기초",
+                    content:
+                      "번수와 자료형, 입·출력 함수, 연산자, 제어문, 배열, 함수, 포인터, 구조체와 공동체, 파일 입출력",
+                  },
+                  {
+                    step3: "프로그래밍 설계와 구현",
+                    content:
+                      "프로그램 기획, 프로그램 설계, 프로그램 구현, 프로그램 테스트",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            grade: 2,
+            depart: [
+              {
+                departTitle: "자료구조",
+                choose: "Public",
+                chapter: [
+                  { step1: "자료", content: "자료와 정보, 자료의 종류와 표현" },
+                  { step2: "선형 구조", content: "배열, 스택, 큐, 리스트" },
+                  { step3: "비선형 구조", content: "트리, 그래프" },
+                  {
+                    step4: "알고리즘",
+                    content: "문제 해결 과정, 자료의 정렬, 자료의 탐색",
+                  },
+                  {
+                    step5: "데이터베이스",
+                    content:
+                      "데이터베이스 개념, 데이터베이스 구조, 데이터베이스 관리",
+                  },
+                ],
+              },
+              {
+                departTitle: "정보 처리와 관리",
+                choose: "Public",
+                chapter: [
+                  {
+                    step1: "정보 산업과 정보 윤리",
+                    content:
+                      "정보 산업의 발전, 산업과 정보 기술, 정보 윤리와 정보 보호",
+                  },
+                  {
+                    step2: "드프로세서 실무",
+                    content: "기본 문서 작성, 공문서 작성, 실무 문서 작성",
+                  },
+                  {
+                    step3: "스프레드시트 실무",
+                    content:
+                      "기본 시트 작성, 실무 문서 작성, 데이터 분석과 보고서 작성",
+                  },
+                  {
+                    step4: "프레젠테이션 실무",
+                    content:
+                      "제안서 작성, 프레젠테이션 제작, 프레젠테이션 발표",
+                  },
+                  {
+                    step5: "데이터베이스 실무",
+                    content: "데이터베이스 작성, 보고서 작성, 자동화 문서",
+                  },
+                  {
+                    step1: "문서 공유와 활용",
+                    content:
+                      "온라인 공동 문서 작성과 공유하기, 스마트 기기와 문서 활용",
+                  },
+                ],
+              },
+              {
+                departTitle: "응용 프로그래밍 화면 구현",
+                choose: "Public",
+                chapter: [{ step1: "미정", content: "미정" }],
+              },
+              {
+                departTitle: "미디어 콘텐츠 일반",
+                choose: "웹 제작 코스",
+                chapter: [{ step1: "미정", content: "미정" }],
+              },
+              {
+                departTitle: "응용 프로그래밍 개발",
+                choose: "소프트웨어 개발 코스",
+                chapter: [
+                  {
+                    step1: "미디어 콘텐츠의 이해",
+                    content:
+                      "미디어 콘텐츠의 개념, 미디어 콘텐츠 관련 기술, 미디어 콘텐츠 활용 분야",
+                  },
+                  {
+                    step2: "미디어의 구성 요소",
+                    content: "문자, 이미지, 소리, 애니메이션, 동영상",
+                  },
+                  {
+                    step3: "미디어 콘텐츠 기획",
+                    content:
+                      "미디어 콘텐츠 기획의 이해, 아이디어 발상 기법, 프로젝트 기획",
+                  },
+                  {
+                    step4: "미디어 콘텐츠 기획 실무",
+                    content:
+                      "시나리오 구조 설계, 스토리보드 작성, 미디어 콘텐츠 기획 발표",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            grade: 3,
+            depart: [
+              {
+                departTitle: "응용 프로그래밍 화면 구현",
+                choose: "Public",
+                chapter: [{ step1: "미정", content: "미정" }],
+              },
+              {
+                departTitle: "데이터베이스 프로그래밍",
+                choose: "Public",
+                chapter: [{ step1: "미정", content: "미정" }],
+              },
+              {
+                departTitle: "스마트 문화 앱 콘텐츠 제작",
+                choose: "Public",
+                chapter: [{ step1: "미정", content: "미정" }],
+              },
+              {
+                departTitle: "응용 프로그래밍 개발",
+                choose: "소프트웨어 개발 코스",
+                chapter: [{ step1: "미정", content: "미정" }],
+              },
+              {
+                departTitle: "전자 상거래 실무",
+                choose: "웹 제작 코스",
+                chapter: [
+                  { step1: "chapter1", content: "사이트 구축" },
+                  { step2: "chapter2", content: "사이트 운영" },
+                  { step3: "chapter3", content: "판매 관리" },
+                  { step4: "chapter4", content: "고객 관리" },
+                  { step5: "chapter5", content: "사후 관리" },
+                ],
+              },
+              {
+                departTitle: "기업과 경영",
+                choose: "Public",
+                chapter: [
+                  {
+                    step1: "기업 경영과 경영 혁신",
+                    content:
+                      "기업 경영의 의의, 기업의 경영 환경, 경영 혁신과 윤리 경영",
+                  },
+                  { step2: "경영 관리", content: "계획, 조직, 지휘, 통제" },
+                  {
+                    step3: "기업의 부문별 활동",
+                    content:
+                      "인적 자원 관리, 생산 관리, 마케팅 관리, 재무 관리, 회계 정보 관리",
+                  },
+                  {
+                    step4: "글로벌 경영",
+                    content:
+                      "기업의 국제 활동, 기업의 국제화와 글로벌 경영, 경제 통합과 FTA",
+                  },
+                ],
+              },
+              {
+                departTitle: "빅데이터 분석",
+                choose: "Public",
+                chapter: [{ step1: "미정", content: "미정" }],
+              },
+              {
+                departTitle: "매장 판매",
+                choose: "Public",
+                chapter: [
+                  { step1: "chapter1", content: "판매 기본 정보 분석" },
+                  {
+                    step2: "chapter2",
+                    content: "매장 영업 전 판매 지원 및 준비 점검",
+                  },
+                  { step3: "chapter3", content: "매장판매 상담" },
+                  { step4: "chapter4", content: "매장상품 판매 마무리" },
+                  { step5: "chapter5", content: "매장고객관리" },
+                ],
+              },
+              {
+                departTitle: "패션 상품 유통 관리",
+                choose: "Public",
+                chapter: [{ step1: "미정", content: "미정" }],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        descTitle: "앱 서비스",
+        grade: [
+          {
+            grade: 1,
+            depart: [
+              {
+                departTitle: "컴퓨터 그래픽",
+                choose: "Public",
+                chapter: [
+                  {
+                    step1: "컴퓨터 그래픽의 이해",
+                    content:
+                      "그래픽 디자인의 범위와 영역, 컴퓨터 그래픽의 발달, 컴퓨터 그래픽의 제작 원리와 요소, 컴퓨터 그래픽의 역할",
+                  },
+                  {
+                    step2: "컴퓨터 그래픽의 원리",
+                    content:
+                      "컴퓨터 그래픽 시스템의 이해, 컴퓨터 그래픽을 통한 시각 커뮤니케이션",
+                  },
+                  {
+                    step3: "컴퓨터 그래픽 요소 제작",
+                    content:
+                      "디지털 컬러 시스템, 2차언 이미지 표현, 3차원 조형물 표현, 움직이는 형태 표현, 인터페이스 디자인 표현",
+                  },
+                  {
+                    step4: "컴퓨터 그래픽 디자인의 활용",
+                    content:
+                      "디지털 이미지 디자인, 편집 디자인, 웹디자인, 애니메이션 디자인, 가상 현실(VR) 디자인",
+                  },
+                ],
+              },
+              {
+                departTitle: "컴퓨터 시스템 일반",
+                choose: "Public",
+                chapter: [
+                  {
+                    step1: "정보의 표현",
+                    content:
+                      "수의 체계, 디지털 정보의 연산, 디지털 정보의 표현",
+                  },
+                  {
+                    step2: "컴퓨팅 시스템",
+                    content:
+                      "컴퓨팅 시스템의 구성과 동작, 논리 연산과 논리 회로, 네트워크의 이해",
+                  },
+                  {
+                    step3: "운영 체제의 자원 관리",
+                    content:
+                      "운영 체제의 구성, 프로세스 관리, 주기억 장치 관리, 보조 기억 장치 관리, 가상 기억 장치 관리, 입출력과 파일 관리",
+                  },
+                  {
+                    step4: "운영 체제 운용",
+                    content:
+                      "리눅스의 이해, 기본 명령어, 사용자 관리와 파일 속성, 리눅스 관리자 명령어, 네트워크 설정 명령어",
+                  },
+                  {
+                    step4: "문제 해결과 알고리즘",
+                    content: "추상화, 알고리즘 설계",
+                  },
+                ],
+              },
+              {
+                departTitle: "프로그래밍",
+                choose: "Public",
+                chapter: [
+                  {
+                    step1: "프로그래밍의 개요",
+                    content:
+                      "프로그래밍 언어의 종류와 특징, 프로그래밍의 절차, 알고리즘, 프로그래밍 개발 환경",
+                  },
+                  {
+                    step2: "프로그래밍의 기초",
+                    content:
+                      "번수와 자료형, 입·출력 함수, 연산자, 제어문, 배열, 함수, 포인터, 구조체와 공동체, 파일 입출력",
+                  },
+                  {
+                    step3: "프로그래밍 설계와 구현",
+                    content:
+                      "프로그램 기획, 프로그램 설계, 프로그램 구현, 프로그램 테스트",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            grade: 2,
+            depart: [
+              {
+                departTitle: "자료구조",
+                choose: "Public",
+                chapter: [
+                  { step1: "자료", content: "자료와 정보, 자료의 종류와 표현" },
+                  { step2: "선형 구조", content: "배열, 스택, 큐, 리스트" },
+                  { step3: "비선형 구조", content: "트리, 그래프" },
+                  {
+                    step4: "알고리즘",
+                    content: "문제 해결 과정, 자료의 정렬, 자료의 탐색",
+                  },
+                  {
+                    step5: "데이터베이스",
+                    content:
+                      "데이터베이스 개념, 데이터베이스 구조, 데이터베이스 관리",
+                  },
+                ],
+              },
+              {
+                departTitle: "정보 처리와 관리",
+                choose: "Public",
+                chapter: [
+                  {
+                    step1: "정보 산업과 정보 윤리",
+                    content:
+                      "정보 산업의 발전, 산업과 정보 기술, 정보 윤리와 정보 보호",
+                  },
+                  {
+                    step2: "드프로세서 실무",
+                    content: "기본 문서 작성, 공문서 작성, 실무 문서 작성",
+                  },
+                  {
+                    step3: "스프레드시트 실무",
+                    content:
+                      "기본 시트 작성, 실무 문서 작성, 데이터 분석과 보고서 작성",
+                  },
+                  {
+                    step4: "프레젠테이션 실무",
+                    content:
+                      "제안서 작성, 프레젠테이션 제작, 프레젠테이션 발표",
+                  },
+                  {
+                    step5: "데이터베이스 실무",
+                    content: "데이터베이스 작성, 보고서 작성, 자동화 문서",
+                  },
+                  {
+                    step1: "문서 공유와 활용",
+                    content:
+                      "온라인 공동 문서 작성과 공유하기, 스마트 기기와 문서 활용",
+                  },
+                ],
+              },
+              {
+                departTitle: "응용 프로그래밍 화면 구현",
+                choose: "Public",
+                chapter: [{ step1: "미정", content: "미정" }],
+              },
+              {
+                departTitle: "미디어 콘텐츠 일반",
+                choose: "웹 제작 코스",
+                chapter: [{ step1: "미정", content: "미정" }],
+              },
+              {
+                departTitle: "응용 프로그래밍 개발",
+                choose: "소프트웨어 개발 코스",
+                chapter: [
+                  {
+                    step1: "미디어 콘텐츠의 이해",
+                    content:
+                      "미디어 콘텐츠의 개념, 미디어 콘텐츠 관련 기술, 미디어 콘텐츠 활용 분야",
+                  },
+                  {
+                    step2: "미디어의 구성 요소",
+                    content: "문자, 이미지, 소리, 애니메이션, 동영상",
+                  },
+                  {
+                    step3: "미디어 콘텐츠 기획",
+                    content:
+                      "미디어 콘텐츠 기획의 이해, 아이디어 발상 기법, 프로젝트 기획",
+                  },
+                  {
+                    step4: "미디어 콘텐츠 기획 실무",
+                    content:
+                      "시나리오 구조 설계, 스토리보드 작성, 미디어 콘텐츠 기획 발표",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            grade: 3,
+            depart: [
+              {
+                departTitle: "응용 프로그래밍 화면 구현",
+                choose: "Public",
+                chapter: [{ step1: "미정", content: "미정" }],
+              },
+              {
+                departTitle: "데이터베이스 프로그래밍",
+                choose: "Public",
+                chapter: [{ step1: "미정", content: "미정" }],
+              },
+              {
+                departTitle: "스마트 문화 앱 콘텐츠 제작",
+                choose: "Public",
+                chapter: [{ step1: "미정", content: "미정" }],
+              },
+              {
+                departTitle: "응용 프로그래밍 개발",
+                choose: "소프트웨어 개발 코스",
+                chapter: [{ step1: "미정", content: "미정" }],
+              },
+              {
+                departTitle: "전자 상거래 실무",
+                choose: "웹 제작 코스",
+                chapter: [
+                  { step1: "chapter1", content: "사이트 구축" },
+                  { step2: "chapter2", content: "사이트 운영" },
+                  { step3: "chapter3", content: "판매 관리" },
+                  { step4: "chapter4", content: "고객 관리" },
+                  { step5: "chapter5", content: "사후 관리" },
+                ],
+              },
+              {
+                departTitle: "기업과 경영",
+                choose: "Public",
+                chapter: [
+                  {
+                    step1: "기업 경영과 경영 혁신",
+                    content:
+                      "기업 경영의 의의, 기업의 경영 환경, 경영 혁신과 윤리 경영",
+                  },
+                  { step2: "경영 관리", content: "계획, 조직, 지휘, 통제" },
+                  {
+                    step3: "기업의 부문별 활동",
+                    content:
+                      "인적 자원 관리, 생산 관리, 마케팅 관리, 재무 관리, 회계 정보 관리",
+                  },
+                  {
+                    step4: "글로벌 경영",
+                    content:
+                      "기업의 국제 활동, 기업의 국제화와 글로벌 경영, 경제 통합과 FTA",
+                  },
+                ],
+              },
+              {
+                departTitle: "빅데이터 분석",
+                choose: "Public",
+                chapter: [{ step1: "미정", content: "미정" }],
+              },
+              {
+                departTitle: "매장 판매",
+                choose: "Public",
+                chapter: [
+                  { step1: "chapter1", content: "판매 기본 정보 분석" },
+                  {
+                    step2: "chapter2",
+                    content: "매장 영업 전 판매 지원 및 준비 점검",
+                  },
+                  { step3: "chapter3", content: "매장판매 상담" },
+                  { step4: "chapter4", content: "매장상품 판매 마무리" },
+                  { step5: "chapter5", content: "매장고객관리" },
+                ],
+              },
+              {
+                departTitle: "패션 상품 유통 관리",
+                choose: "Public",
+                chapter: [{ step1: "미정", content: "미정" }],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
