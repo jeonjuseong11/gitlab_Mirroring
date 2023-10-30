71f5b888944a685471da3e6ec78bfac5465734bc
WePung <akgre5@naver.com>
2023-10-30 23:48:32 +0900

    Edit : FeedbackDetail CSS
    CSS 수정

--- highway/src/components/Feedback/FeedbackDetail.js
+++ highway/src/components/Feedback/FeedbackDetail.js
@@ -1,40 +1,66 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { EllipsisOutlined } from "@ant-design/icons";
 import {
-  LOAD_FEEDBACK_LIST_REQUEST,
+  Breadcrumb,
+  Button,
+  Col,
+  Dropdown,
+  Menu,
+  Modal,
+  Row,
+  Spin,
+} from "antd";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import {
+  LOAD_FEEDBACK_REQUEST,
+  LOAD_USER_REQUEST,
   REMOVE_FEEDBACK_REQUEST,
-  UPDATE_FEEDBACK_REQUEST,
 } from "../../constants/actionTypes";
+import { changeCategory } from "../../pages/Board/BoardMain";
+import {
+  AuthorInfo,
+  AuthorName,
+  PostContent,
+  PostDate,
+  PostHeader,
+  PostTitle,
+  StyledAvatar,
+  StyledBreadcrumbWrapper,
+} from "../../styles/BoardStyle";
+import { formatDate } from "../schoolBoardDetail/ToggleComment";
+import FeedbackResponseInput from "./FeedbackResponseInput";
 
 const FeedbackDetail = () => {
   const { id } = useParams();
-  const { feedbackList } = useSelector((state) => state.feedback);
+  const { feedback, feedbackLoadLoading } = useSelector(
+    (state) => state.feedback
+  );
+  const { me } = useSelector((state) => state.user);
   const dispatch = useDispatch();
-  const feedbackListMap = feedbackList.map((item) => {
-    if (item.id === parseInt(id)) {
-      return item;
-    }
-  });
-
-  const feedbackItem = feedbackListMap.filter((it) => it !== undefined)[0];
-  console.log(feedbackItem);
-
-  const onUpdateFeedback = () => {
-    dispatch({
-      type: UPDATE_FEEDBACK_REQUEST,
-      data: {
-        id: id,
-        title: feedbackItem.title,
-        content: feedbackItem.content,
-        category: feedbackItem.category,
-        response: "임시 수정내용",
-      },
-    });
+  const navigate = useNavigate();
+  const [canEditOrDelete, setCanEditOrDelete] = useState(false);
+  const [isConfirmModalVisible, setIsConfirmModalVisible] = useState(false);
+  const [confirmAction, setConfirmAction] = useState(null); // 삭제 혹은 수정을 결정하는 state
+  const showConfirmModal = (action) => {
+    setConfirmAction(action); // 현재 액션(수정 혹은 삭제)을 상태로 저장
+    setIsConfirmModalVisible(true);
   };
 
-  const onRemoveFeedback = (values) => {
-    console.log(id);
+  const handleOkConfirm = () => {
+    if (confirmAction === "edit") {
+      navigate(`/`);
+    } else if (confirmAction === "delete") {
+      onRemoveFeedback(id);
+    }
+    setIsConfirmModalVisible(false);
+  };
+
+  const handleCancelConfirm = () => {
+    setIsConfirmModalVisible(false);
+  };
+
+  const onRemoveFeedback = (id) => {
     dispatch({
       type: REMOVE_FEEDBACK_REQUEST,
       data: {
@@ -45,31 +71,126 @@ const FeedbackDetail = () => {
 
   useEffect(() => {
     dispatch({
-      type: LOAD_FEEDBACK_LIST_REQUEST,
+      type: LOAD_USER_REQUEST,
     });
+    dispatch({
+      type: LOAD_FEEDBACK_REQUEST,
+      data: {
+        id: id,
+      },
+    });
+    console.log(me);
   }, []);
   return (
-    <div>
-      <div>제목 : {feedbackItem?.title}</div>
-      <div>내용 : {feedbackItem?.content}</div>
-      <div>카테고리 : {feedbackItem?.category}</div>
-      <div>답변 : {feedbackItem?.response}</div>
-      <div>
-        <button
-          onClick={() => {
-            onUpdateFeedback();
-          }}
-        >
-          업데이트
-        </button>
-        <button
-          onClick={() => {
-            onRemoveFeedback();
-          }}
-        >
-          삭제
-        </button>
-      </div>
+    <div
+      style={{
+        maxWidth: "65rem",
+        width: "100%",
+        marginTop: "0",
+        marginBottom: "0",
+        marginLeft: "auto",
+        marginRight: "auto",
+      }}
+    >
+      <Row gutter={[16, 16]} justify="center" style={{ marginTop: "1rem" }}>
+        <Col xs={23} md={24} style={{ textAlign: "left" }}>
+          {feedbackLoadLoading ? (
+            <div
+              style={{
+                textAlign: "center",
+                height: "10rem",
+                justifyContent: "center",
+                alignItems: "center",
+                display: "flex",
+              }}
+            >
+              <Spin size="large" />
+            </div>
+          ) : (
+            <>
+              <StyledBreadcrumbWrapper>
+                <Breadcrumb
+                  items={[
+                    {
+                      title: <a href={`/schoolboard/0`}>문의 종류</a>,
+                    },
+                    {
+                      title: (
+                        <>
+                          <a href={`/schoolboard/${id}`}>
+                            {changeCategory(feedback?.category)}
+                          </a>
+                        </>
+                      ),
+                    },
+                  ]}
+                />
+              </StyledBreadcrumbWrapper>
+              <PostHeader>
+                <PostTitle>{feedback?.title}</PostTitle>
+                <StyledAvatar size={32}>{feedback?.userName}</StyledAvatar>
+                <AuthorInfo>
+                  <AuthorName>{feedback?.userName}</AuthorName>
+                  <br />
+                  <PostDate>{formatDate(feedback?.createDate)}</PostDate>
+                </AuthorInfo>
+                {canEditOrDelete && (
+                  <Dropdown
+                    placement="bottomLeft"
+                    overlay={
+                      <Menu>
+                        <Menu.Item
+                          onClick={() => showConfirmModal("edit")} // 액션 타입을 전달하여 모달을 띄웁니다.
+                        >
+                          수정하기
+                        </Menu.Item>
+                        <Menu.Item
+                          danger
+                          onClick={() => showConfirmModal("delete")} // 액션 타입을 전달하여 모달을 띄웁니다.
+                        >
+                          삭제하기
+                        </Menu.Item>
+                      </Menu>
+                    }
+                    trigger={["hover"]}
+                  >
+                    <EllipsisOutlined style={{ float: "right" }} />
+                  </Dropdown>
+                )}
+              </PostHeader>
+              <PostContent
+                dangerouslySetInnerHTML={{
+                  __html: feedback?.content,
+                }}
+              />
+
+              <Modal
+                title={confirmAction === "edit" ? "수정 확인" : "삭제 확인"}
+                open={isConfirmModalVisible}
+                onOk={handleOkConfirm}
+                onCancel={handleCancelConfirm}
+                centered={true}
+                okButtonProps={{
+                  danger: confirmAction === "delete",
+                  style:
+                    confirmAction === "delete"
+                      ? { backgroundColor: "red" }
+                      : {},
+                }}
+                // okButtonProps를 사용하여 삭제 동작일 때 '확인' 버튼을 빨간색으로 만듭니다.
+              >
+                <p>
+                  {confirmAction === "edit"
+                    ? "정말로 이 게시글을 수정하시겠습니까?"
+                    : "정말로 이 게시글을 삭제하시겠습니까?"}
+                </p>
+              </Modal>
+            </>
+          )}
+        </Col>
+        {me?.role !== 0 ? <FeedbackResponseInput feedback={feedback} /> : <></>}
+        {feedback.response ? <>{feedback.response}</> : <></>}
+      </Row>
     </div>
   );
 };
