ff4b981f56e6f4f03b128e2a494e1a497c7ccdf4
WePung <akgre5@naver.com>
2023-11-01 11:09:27 +0900

    Add : FeedbackStyle
    피드백 스타일 컴포넌트 추가

---
+++ highway/src/styles/FeedbackStyle.js
@@ -0,0 +1,10 @@
+import { Col } from "antd";
+import styled from "styled-components";
+
+// FeedbackList
+export const ListCol = styled(Col)`
+  cursor: pointer;
+  font-size: 20px;
+  margin-top: 5px;
+  margin-bottom: 5px;
+`;
